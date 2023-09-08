206d51bf74528f3adc1323fad6f293c251031826
Elvis Benites <enekc123@gmail.com>
2023-09-08 12:09:04 -0500

    feat(FE): add component for modal

--- frontend/ft-social/src/main.tsx
+++ frontend/ft-social/src/main.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import Modal  from './shared/components/InterestChosen/index.tsx'
 import './index.css'
 import { Switch } from '@mui/material'
 import { Route } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Switch>
       <Route path='/modal'>
-        
+        <Modal></Modal>
       </Route>
     </Switch>
     <App />
--- frontend/ft-social/src/shared/components/InterestChosen/index.tsx
+++ frontend/ft-social/src/shared/components/InterestChosen/index.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Modal() {
+const Modal: React.FC = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <div className="modal">
-      <h2>Modal Content</h2>
-      {/* Add your modal content here */}
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <h2>Modal Content</h2>
+            {/*STARTING VIEW*/}
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Modal;
