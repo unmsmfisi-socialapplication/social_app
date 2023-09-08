53d20f0b38113a1e8c4f6af1f95e62fbf06c23a6
Adrianabalcd <adribalcd@gmail.com>
2023-09-08 16:50:47 -0500

    Organize folders and files in feature branch

--- backend/multipublication/handler/multipublication.go
+++ backend/multipublication/handler/multipublication.go
@@ -1,1 +1,42 @@
 // file of controller
+
+package handler
+
+import (
+  "context"
+  "fmt"
+  "net/http"
+
+  "github.com/go-fed/activitypub"
+  "github.com/unmsmfisi-socialapplication/social_app/backend/internal/multipublication/application"
+)
+
+// MultipublicationHandler handles requests for multipublication.
+type MultipublicationHandler struct {
+  app *application.MultipublicationApplication
+}
+
+// NewMultipublicationHandler creates a new MultipublicationHandler.
+func NewMultipublicationHandler(app *application.MultipublicationApplication) *MultipublicationHandler {
+  return &MultipublicationHandler{app}
+}
+
+// Handle handles a request for multipublication.
+func (h *MultipublicationHandler) Handle(w http.ResponseWriter, r *http.Request) {
+  // Get the publication data.
+  data := struct {
+    Text string `json:"text"`
+  }{}
+
+  err := json.NewDecoder(r.Body).Decode(&data)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+
+  // Publish the publication on the configured social networks.
+  h.app.Publish(context.Background(), data.Text)
+
+  // Return a successful response.
+  w.WriteHeader(http.StatusOK)
+}
--- backend/multipublication/usecase/multipublication.go
+++ backend/multipublication/usecase/multipublication.go
@@ -1,1 +1,57 @@
 // file of use
+
+package usecase
+
+import (
+  "context"
+  "errors"
+  "github.com/go-fed/activitypub"
+  "github.com/unmsmfisi-socialapplication/social_app/backend/internal/multipublication/application"
+)
+
+// MultipublicationApplication is the application for multipublication.
+type MultipublicationApplication struct {
+  config *config.MultipublicationConfig
+  client activitypub.Client
+}
+
+// NewMultipublicationApplication creates a new MultipublicationApplication.
+func NewMultipublicationApplication(config *config.MultipublicationConfig, client activitypub.Client) *MultipublicationApplication {
+  return &MultipublicationApplication{
+    config: config,
+    client: client,
+  }
+}
+
+// Publish publishes a publication on the configured social networks.
+func (app *MultipublicationApplication) Publish(ctx context.Context, text string) error {
+  // Get the authorization tokens for the configured social networks.
+  tokens := []string{}
+  for _, network := range app.config.Networks {
+    token, err := app.client.GetToken(network)
+    if err != nil {
+      return err
+    }
+
+    tokens = append(tokens, token)
+  }
+
+  // Publish the publication on each social network.
+  for _, token := range tokens {
+    // Create a new publication.
+    publication := activitypub.NewActivityPubObject(activitypub.ObjectTypeActivity)
+    publication.Id = activitypub.NewID()
+    publication.Actor = activitypub.NewActor(app.client.Me())
+    publication.Object = activitypub.NewLink(publication.Id)
+    publication.Published = time.Now()
+    publication.Content = activitypub.NewString(text)
+
+    // Publish the publication.
+    err := app.client.Post(token, publication)
+    if err != nil {
+      return err
+    }
+  }
+
+  return nil
+}
