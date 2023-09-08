58788ac5a3512aa550ab05cbe32d7647fd3c1dca
Elvis Benites <enekc123@gmail.com>
2023-09-08 12:05:09 -0500

    feat(FE):add router link

--- frontend/ft-social/src/main.tsx
+++ frontend/ft-social/src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Switch } from '@mui/material'
+import { Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Switch>
+      <Route path='/modal'>
+        
+      </Route>
+    </Switch>
     <App />
   </React.StrictMode>,
 )
---
+++ frontend/ft-social/src/shared/components/InterestChosen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>Modal Content</h2>
+      {/* Add your modal content here */}
+    </div>
+  );
+}
+
+export default Modal;
