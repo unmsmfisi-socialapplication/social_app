a267273a1ffed0950cdfed99b9530af31983429a
Ricse Anthony <anthony.ricse@unmsm.edu.pe>
2023-09-08 00:19:56 -0500

    feat(button&&preprocesador):add type button color and configuration less var

--- frontend/ft-social/src/App.tsx
+++ frontend/ft-social/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react'
 export const ChatModule = React.lazy(() => import('./shared/components/chat'));
+import { Button } from './shared/components';
 
 import './App.css'
 
@@ -13,6 +14,7 @@ function App() {
       {/* Usa Suspense para envolver el componente perezoso */}
       <Suspense fallback={<div>Cargando...</div>}>
         <ChatModule />
+        <Button />
       </Suspense>
     </>
   )
---
+++ frontend/ft-social/src/shared/components/button/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import './index.less';
+
+type WButtonProps = {
+    typeColor: 'primary' | 'secondary' | 'tertiary';
+    text: string; // Definir que text es de tipo string
+    size: 'small' | 'medium' | 'large' ;
+};
+  
+  const WButton: React.FC<WButtonProps> = ({ typeColor , text ,size}) => {
+    return (
+      <Button
+        className={`custom-button ${typeColor}`}
+        size={size}
+      >
+        {text}
+      </Button>
+    );
+  };
+  
+export default WButton;
+
+WButton.defaultProps = {
+    typeColor: 'secondary',
+    text: 'Default text',
+    size: undefined,
+};
