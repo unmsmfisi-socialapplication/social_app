791c9e5fe113b248110c03547ba9040e72c358be
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-09-08 20:24:02 -0500

    test(profile): Add test to import profile use case

---
+++ backend/internal/profile/test/importProfileUseCase_test.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/unmsmfisi-socialapplication/social_app/internal/profile/application"
+	"github.com/unmsmfisi-socialapplication/social_app/internal/profile/domain"
+	"github.com/unmsmfisi-socialapplication/social_app/internal/profile/infrastructure"
+)
+
+func TestImportProfileUseCase(t *testing.T) {
+	t.Log("TestImportProfileUseCase")
+
+	p := &domain.Profile{
+		Id_profile:     1,
+		Username:       "test",
+		ProfilePicture: "test",
+		Biography:      "test",
+	}
+
+	var profile *domain.Profile
+
+	profileRepositoryTest := infrastructure.NewProfileRepositoryTest()
+	importProfileUseCase := application.NewImportProfileUseCase(profileRepositoryTest)
+
+	profile, err := importProfileUseCase.ImportProfile(p)
+
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	if profile.Username != p.Username {
+		t.Errorf("Error: %v", err)
+	}
+
+	if profile.ProfilePicture != p.ProfilePicture {
+		t.Errorf("Error: %v", err)
+	}
+
+	if profile.Biography != p.Biography {
+		t.Errorf("Error: %v", err)
+	}
+
+	t.Log("TestImportProfileUseCase: Success")
+}
