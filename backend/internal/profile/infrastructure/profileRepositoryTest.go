a50408c699adeaf143d344fade26681c8acc5d6d
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2023-09-08 20:22:59 -0500

    test(profile): add repository for testing profileRepositoryTest

---
+++ backend/internal/profile/infrastructure/profileRepositoryTest.go
@@ -0,0 +1,13 @@
+package infrastructure
+
+import "github.com/unmsmfisi-socialapplication/social_app/internal/profile/domain"
+
+type ProfileRepositoryTest struct {}
+
+func NewProfileRepositoryTest() *ProfileRepositoryTest {
+	return &ProfileRepositoryTest{}
+}
+
+func (pr *ProfileRepositoryTest) UpdateProfile(profile *domain.Profile) (*domain.Profile, error) {
+	return profile, nil
+}
