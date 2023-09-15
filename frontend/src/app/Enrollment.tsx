8f7bf2bca880297a7b2ee387021379c3326838aa
wilfredo huallpartupa <wilfredo.huallpartupa@panbox.io>
2023-09-15 11:43:27 -0500

    Corrección del código de Enrollment.tsx

--- frontend/src/app/Enrollment.tsx
+++ frontend/src/app/Enrollment.tsx
@@ -6,12 +6,18 @@ export default function RootEnrollment({
   }: {
     children: React.ReactNode
   }) {
+    const divStyle = {
+      margin: '0px',
+      padding: '0px',
+      background: `url('/images/background.jpg')`, 
+      backgroundSize: 'cover', 
+    };
+
+    
     return (
       <html lang="en">
-        <body style={{margin: "0px", padding: "0px"}}>
-            <img
-                src='/images/background.png'
-            ></img>
+        <body style={divStyle}>
+          {children}
         </body>
       </html>
     )
