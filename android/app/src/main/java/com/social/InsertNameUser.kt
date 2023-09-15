933154bf786101e5c6661b2f60812a8a178fec1d
Jeanpiere Julian Palacios Barrutia <87631120+JeanpierePalacios@users.noreply.github.com>
2023-09-15 16:17:17 -0500

    Implement SharedPreferences
    Implement SharedPreferences to persistently store user data, such as profile photo and name, so that this data is preserved even when the user changes activities.

--- android/app/src/main/java/com/social/InsertNameUser.kt
+++ android/app/src/main/java/com/social/InsertNameUser.kt
@@ -3,8 +3,9 @@ package com.social
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.ImageButton
 import android.content.Intent
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
 import android.widget.Button
 import android.widget.EditText
 import android.view.View
@@ -12,44 +13,49 @@ import android.text.Editable
 import android.text.TextWatcher
 
 class InsertNameUser : AppCompatActivity() {
+    lateinit var userPhoto_select: ImageView
+    lateinit var btnBack_uploadP: Button
+    lateinit var btnAccept_name: Button
+    lateinit var inputName: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_insert_name_user)
 
-        val profileImageView = findViewById<ImageView>(R.id.userFoto_name)
-        val ibtnAtras = findViewById<ImageButton>(R.id.ibtnAtras)
-        val inputName = findViewById<EditText>(R.id.inputName)
-        val btnAceptar = findViewById<Button>(R.id.btnAceptar)
+        userPhoto_select = findViewById(R.id.userPhoto_select)
+        btnBack_uploadP = findViewById(R.id.btnBack_uploadP)
+        inputName = findViewById(R.id.inputName)
+        btnAccept_name = findViewById(R.id.btnAccept_name)
 
-        // Obtener la imagen pasada desde MainActivity
-        val selectedImageUriString = intent.getStringExtra("selectedImageUri")
+        // Obtener la imagen pasada desde UploadPhotoUser en SharedPreferences
+        val sharedPreferences: SharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(this)
+        val selectedImageUriString = sharedPreferences.getString("selectedImageUri", "")
 
         // Si se pasó una imagen, establecerla en el ImageView
         if (!selectedImageUriString.isNullOrEmpty()) {
             val selectedImageUri = android.net.Uri.parse(selectedImageUriString)
-            profileImageView.setImageURI(selectedImageUri)
+            userPhoto_select.setImageURI(selectedImageUri)
         }
 
-        // Volver
-        ibtnAtras.setOnClickListener {
-            // Crear un Intent para volver a MainActivity
-            val intent = Intent(this, MainActivity::class.java)
+        // Regresar a la View para subir foto de perfil
+        btnBack_uploadP.setOnClickListener {
+            // Crear un Intent para volver a UploadPhotoUser
+            val intent = Intent(this, UploadPhotoUser::class.java)
             startActivity(intent)
             finish() // Cierra la actividad actual para que no quede en segundo plano
         }
 
-        //
         inputName.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 // No es necesario implementar este método
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // Comprueba si el texto del EditText no está vacío y muestra el botón si es cierto
                 if (!s.isNullOrEmpty()) {
-                    btnAceptar.visibility = View.VISIBLE
+                    btnAccept_name.visibility = View.VISIBLE
                 } else {
-                    btnAceptar.visibility = View.INVISIBLE
+                    btnAccept_name.visibility = View.INVISIBLE
                 }
             }
 
@@ -57,5 +63,27 @@ class InsertNameUser : AppCompatActivity() {
                 // No es necesario implementar este método
             }
         })
+
+        btnAccept_name.setOnClickListener {
+            // Obtener el texto ingresado en el EditText
+            val name = inputName.text.toString()
+
+            // Obtener la imagen pasada desde UploadPhotoUser en SharedPreferences
+            val sharedPreferences: SharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this)
+            val selectedImageUriString = sharedPreferences.getString("selectedImageUri", "")
+
+            // Si se pasó una imagen, establecerla en el ImageView de InsertBibliographyUser
+            if (!selectedImageUriString.isNullOrEmpty()) {
+                val selectedImageUri = android.net.Uri.parse(selectedImageUriString)
+
+                // Crear un Intent para ir a InsertBibliographyUser y pasar la imagen y el nombre
+                val intent = Intent(this, InsertBibliographyUser::class.java)
+                intent.putExtra("selectedImageUri", selectedImageUri.toString())
+                intent.putExtra("name", name)
+                startActivity(intent)
+            }
+        }
+
     }
 }
