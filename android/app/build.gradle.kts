2c344bb3ca9ab4c9c5be91f387842ac86d04dc65
JoseCalle <jose.callesoraluz@unmsm.edu.pe>
2023-09-20 06:21:08 -0500

    project restructuring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,20 +1,27 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
 }
 
 android {
     namespace = "com.social"
-    compileSdk = 33
+    compileSdk = 34
+
+    useLibrary("org.apache.http.legacy")
 
     defaultConfig {
         applicationId = "com.social"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -33,11 +40,58 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.0"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    viewBinding{
+        enable = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
+    val roomVersion = "2.5.2"
+    val composeUiVersion = "1.5.1"
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
+
+    //Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeUiVersion")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeUiVersion")
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    // Room
+    implementation("androidx.room:room-runtime:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    //kotlin extensiones
+    implementation("androidx.room:room-ktx:$roomVersion")
+    //picasso
+    implementation("com.squareup.picasso:picasso:2.71828")
+
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+}
+
+buildscript {
+    dependencies {
+        val navegacionVersion="2.7.2"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navegacionVersion")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.47")
+        classpath("com.google.gms:google-services:4.4.0")
+    }
 }
