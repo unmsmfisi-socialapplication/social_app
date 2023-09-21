40b7a65e514fbda3ff07559dbadad1242e5ccba3
wilfredo huallpartupa <wilfredo.huallpartupa@panbox.io>
2023-09-20 23:04:05 -0500

    Atomic InputCode and molecule verificationCode

---
+++ frontend/src/components/molecules/VerificationCode/verificationCode.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import WInputCode from '@/components/atoms/InputCode/inputCode';
+
+interface WVerificationCodeProps {
+  typeColor?: 'primary' | 'secondary';
+  placeholder?: string;
+  size?: 'small' | 'medium';
+  variant?: 'standard' | 'filled' | 'outlined';
+  fullWidth?: boolean;
+  type?: 'text' | 'password';
+}
+
+const WVerificationCode: React.FC<WVerificationCodeProps> = ({
+  typeColor = 'primary',
+  size = 'medium',
+  variant = 'outlined',
+  fullWidth = false,
+  type = 'text',
+}) => {
+
+    const divStyle = {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "30px"
+      }; 
+
+  return (
+    <div style={divStyle}>
+        <WInputCode/>
+        <WInputCode/>
+        <WInputCode/>
+        <WInputCode/>
+    </div>
+  );
+};
+
+export default WVerificationCode;
