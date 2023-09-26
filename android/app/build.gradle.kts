2630cfb18f83fc2c91152e328c42496b5876343d
JoseCalle <jose.callesoraluz@unmsm.edu.pe>
2023-09-25 23:13:50 -0500

    adding dependences to fix bugs about version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -64,16 +64,11 @@ android {
 
 dependencies {
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.3")
+
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.3")
+
     val roomVersion = "2.5.2"
     val composeUiVersion = "1.5.1"
-
-    implementation("androidx.compose.runtime:runtime:$composeUiVersion")
-    implementation("androidx.compose.runtime:runtime-livedata:$composeUiVersion")
-    implementation("androidx.compose.runtime:runtime-rxjava2:$composeUiVersion")
-    implementation("androidx.compose.ui:ui:$composeUiVersion")
-
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
@@ -95,17 +90,25 @@ dependencies {
     // Room
     implementation("androidx.room:room-runtime:$roomVersion")
     kapt("androidx.room:room-compiler:$roomVersion")
-    //kotlin extensiones
+    //kotlin extenciones
     implementation("androidx.room:room-ktx:$roomVersion")
     //picasso
     implementation("com.squareup.picasso:picasso:2.71828")
-
+    //implementation("com.github.danysantiago:sendgrid-android:1")
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation(platform("androidx.compose:compose-bom:2023.09.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.01"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
