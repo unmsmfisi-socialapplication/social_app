4963be17dfefcd874b87836031772024447bcca1
Cristhofer Ventura <cristhofer.ventura@unmsm.edu.pe>
2023-09-26 19:32:22 -0500

    feat test validate password

--- frontend/src/utilities/Constant.tsx
+++ frontend/src/utilities/Constant.tsx
@@ -1,1 +1,3 @@
 export const nameRegex = /^[a-zA-ZÀ-ÿ\s'-]{1,150}$/;
+
+export const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
--- frontend/src/utilities/Validation.tsx
+++ frontend/src/utilities/Validation.tsx
@@ -1,6 +1,10 @@
-import { nameRegex } from "./Constant";
+import { nameRegex, passwordRegex } from "./Constant";
 
 export const validateName = (inputString: string): boolean => {
     return nameRegex.test(inputString);
+  };
+
+export const validatePassword = (inputString: string): boolean => {
+    return passwordRegex.test(inputString);
   };
   
