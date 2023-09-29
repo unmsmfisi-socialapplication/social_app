bf1b951a050cc582fc9305556a991d939d14b524
unknown <manuel.vallejos@unmsm.edu.pe>
2023-09-29 06:57:30 -0500

    feat: Implement initial comments functionality and improve route handling

--- backend/internal/router.go
+++ backend/internal/router.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 
+	"github.com/unmsmfisi-socialapplication/social_app/internal/comment"
 	"github.com/unmsmfisi-socialapplication/social_app/internal/login/application"
 	"github.com/unmsmfisi-socialapplication/social_app/internal/login/infrastructure"
 	"github.com/unmsmfisi-socialapplication/social_app/pkg/database"
@@ -27,8 +28,10 @@ func Router() http.Handler {
 
 	dbInstance := database.GetDB()
 
-	dbRepo := infrastructure.NewUserDBRepository(dbInstance)
+	commentRouter := comment.CommentModuleRouter(dbInstance)
+	r.Mount("/comments", commentRouter)
 
+	dbRepo := infrastructure.NewUserDBRepository(dbInstance)
 	loginUseCase := application.NewLoginUseCase(dbRepo)
 	loginHandler := infrastructure.NewLoginHandler(loginUseCase)
 
