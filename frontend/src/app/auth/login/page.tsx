f0604685c3c472ed523793e988c6b6049494701a
antruWilker <antru.wilker@gmail.com>
2023-09-29 02:25:47 -0500

    searchcomponent #237-titleSearch

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import EnrollmentHoc from "@/app/auth/auth";
 import CardAuth from "@/components/organisms/CardAuth";
+import LogoTitle from "@/components/atoms/SearchComponents/titleSearch";
 
 import { WInput } from "@/components";
 
@@ -11,6 +12,12 @@ export default function LoginPage() {
         <CardAuth title="testing-title-login-user">
             <span>Email</span>
             <WInput typeColor="primary" placeholder="tesing usario" fullWidth/>
+            <LogoTitle
+        src="/public/images/FrameStudentNET.png" 
+        alt="Mi Imagen"
+        width="200px" 
+        height="30px"
+      />
         </CardAuth>
     </EnrollmentHoc>
   );
--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -3,7 +3,7 @@ export { default as WButton } from './atoms/Button/button';
 export { default as WInput } from './atoms/Input/input';
 
 // export molecule components
-
+export { default as titleSearch} from './atoms/SearchComponents/titleSearch';
 
 
 // export organism components
