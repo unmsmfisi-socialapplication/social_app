2d8e24e3f4a2b69fc7a42380ba0c49635dc8d4b0
Alexander Torre <alexander.torre.arteaga@gmail.com>
2023-09-29 15:19:43 -0500

    force ci

--- backend/internal/router.go
+++ backend/internal/router.go
@@ -21,13 +21,14 @@ func Router() http.Handler {
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
+
 	corsMiddleware := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3000"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
+		MaxAge:           300,
 	})
 
 	r.Use(corsMiddleware.Handler)
