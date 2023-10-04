e5dbbdc7568878e275c42be83c75c19e7e9df30f
francosilva25 <e.franco250299@gmail.com>
2023-10-03 20:56:57 -0500

    [Backend] feat: Implementation of reading of environment variables in a file

--- backend/config.go
+++ backend/config.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type Config struct {
@@ -10,7 +12,49 @@ type Config struct {
 	AppPort            string
 }
 
+func loadEnvFromFile(filename string) {
+	file, _ := os.Open(filename)
+	defer file.Close()
+
+	env := make(map[string]string)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) == 2 {
+			env[parts[0]] = parts[1]
+		}
+	}
+
+	for key, value := range env {
+		os.Setenv(key, value)
+	}
+}
+
+func checkEnvVariables() error {
+	requiredVariables := []string{"DB_HOST", "DB_USER", "DB_PASSWORD", "DB_DBNAME", "DB_SSLMODE"}
+
+	for _, variable := range requiredVariables {
+		fmt.Println(os.Getenv(variable))
+		if os.Getenv(variable) == "" {
+			return fmt.Errorf("Environment variable %s is not set", variable)
+		}
+	}
+
+	return nil
+}
+
 func LoadConfig() *Config {
+
+	loadEnvFromFile(".env")
+
+	err := checkEnvVariables()
+	if err != nil {
+		fmt.Println("Environment variables are incorrectly set")
+		return nil
+	}
+
 	return &Config{
 		DBConnectionString: fmt.Sprintf(
 			"host=%s user=%s password=%s dbname=%s sslmode=%s",
