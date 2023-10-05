99e6ce6ae0f5cb071414d8741a03fd4e9a914e90
JoseCalle <jose.callesoraluz@unmsm.edu.pe>
2023-10-05 15:49:20 -0500

    added the use case for login and another parameter in Resource file

--- android/app/src/main/java/com/social/domain/use_case/ValidateUser.kt
+++ android/app/src/main/java/com/social/domain/use_case/ValidateUser.kt
@@ -1,13 +1,16 @@
 package com.social.domain.use_case
 
 import com.social.data.source.remote.dto.LoginDto
+import com.social.data.source.remote.dto.aLogin
 import com.social.domain.SocialAppRepository
+import com.social.domain.model.InvalidUserException
 import com.social.domain.model.LoginBody
 import com.social.domain.model.LoginUserData
 import com.social.utils.Resource
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.lang.Exception
+import java.net.UnknownHostException
 import javax.inject.Inject
 
 class ValidateUser @Inject constructor(
@@ -16,9 +19,22 @@ class ValidateUser @Inject constructor(
     operator fun invoke(loginBody: LoginBody): Flow<Resource<List<LoginUserData>>> {
         return flow {
             try {
-
-            }catch (e: Exception){
-
+                if(loginBody.username.isBlank()){
+                    throw InvalidUserException("Ingrese usuario")
+                }
+                if(loginBody.password.isBlank()){
+                    throw InvalidUserException("Ingrese contraseña")
+                }
+                emit(Resource.Loading())
+                val login = socialAppRepository.validateUser(loginBody).aLogin()
+                when(login.status){
+                    "OK" -> {emit(Resource.Success(login.response))}
+                    else -> {emit(Resource.Error(login.response))}
+                }
+            }catch (u: UnknownHostException){
+                emit(Resource.Error("No se pudo conectar al servidor"))
+            }catch (e : Exception){
+                emit(Resource.Error(e.message.toString()))
             }
         }
     }
