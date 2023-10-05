97b044b8fe1b7a470d91b00151f8e8e1bea11199
gustavo-guerreros1 <75128349+gustavo-guerreros1@users.noreply.github.com>
2023-10-04 23:51:29 -0500

    sp_create_script_model_Logistic_Regression
    clean References: #260 , @gustavo-guerreros1

--- data/models_testing/spam_detector/sp_Create_script_model_Logistic_Regression.py
+++ data/models_testing/spam_detector/sp_Create_script_model_Logistic_Regression.py
@@ -1,24 +1,16 @@
-# Importar las bibliotecas necesarias
 import pandas as pd
-import string
-import random
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score  
+from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_curve, roc_auc_score
 import matplotlib.pyplot as plt
-import pandas as pd
 import re
 from bs4 import BeautifulSoup
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
-import gdown
 
 # Descargar los recursos de NLTK necesarios
 nltk.download('punkt')
@@ -50,3 +42,50 @@ def clean_text(text):
     # Unir tokens nuevamente
     cleaned_text = ' '.join(tokens)
     return cleaned_text
+
+# Aplicar la función de limpieza a la columna de texto
+data['text_cleaned'] = data['FORMATTED_CONTENT'].apply(clean_text)
+
+# Crear un objeto TF-IDF Vectorizer con un número máximo de características
+max_features = 5000  
+tfidf_vectorizer = TfidfVectorizer(max_features=max_features)
+
+# Ajustar y transformar los datos de texto limpio
+tfidf_matrix = tfidf_vectorizer.fit_transform(data['text_cleaned'])
+
+# Obtener las características TF-IDF en un DataFrame de pandas
+tfidf_df = pd.DataFrame(tfidf_matrix.toarray(), columns=tfidf_vectorizer.get_feature_names_out())
+
+# Dividir los datos en características (X) y etiquetas (y)
+X = tfidf_df  # Características TF-IDF
+y = data['CLASS']  # Etiquetas (clase spam o no spam)
+
+# Dividir los datos en conjuntos de entrenamiento y pruebas (80% entrenamiento, 20% prueba)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Crear un clasificador Naive Bayes
+naive_bayes_classifier = MultinomialNB()
+
+# Entrenar el modelo en el conjunto de entrenamiento
+naive_bayes_classifier.fit(X_train, y_train)
+
+# Realizar predicciones en el conjunto de prueba
+y_pred_naive_bayes = naive_bayes_classifier.predict(X_test)
+
+# Calcular la precisión del modelo
+accuracy_naive_bayes = accuracy_score(y_test, y_pred_naive_bayes)
+
+# Crear un clasificador de Regresión Logística
+logistic_regression_classifier = LogisticRegression()
+
+# Entrenar el modelo en el conjunto de entrenamiento
+logistic_regression_classifier.fit(X_train, y_train)
+
+# Realizar predicciones en el conjunto de prueba
+y_pred_logistic_regression = logistic_regression_classifier.predict(X_test)
+
+# Calcular la precisión del modelo
+accuracy_logistic_regression = accuracy_score(y_test, y_pred_logistic_regression)
+
+print("Precisión del modelo Naive Bayes:", accuracy_naive_bayes)
+print("Precisión del modelo de Regresión Logística:", accuracy_logistic_regression)
