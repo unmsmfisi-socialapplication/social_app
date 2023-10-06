f0536b2f7e9d448a0012a1c8b7b1f7b881f0bd1e
Ricse Anthony <anthony.ricse@unmsm.edu.pe>
2023-10-06 18:51:05 -0500

    fix (listComponent) : add list

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -8,7 +8,7 @@ export { default as WTag} from "./atoms/Tag/tag"
 export { default as WLogo} from './atoms/Logo/titleSearch';
 export { default as WAvatar } from './molecules/AvatarInput/index';
 export { default as WVerificationCode } from './molecules/VerificationCode/verificationCode';
-
+export { default as WComentThink } from './molecules/CommentThink/index';
 
 
 
