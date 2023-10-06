9b26ae0c4dd5c2b5dab7cba28e990e7abd42ec70
Angel Carmen Cruzatti <angelcarmen0302@gmail.com>
2023-10-05 20:27:32 -0500

    [Backend] test: Added unit tests

---
+++ backend/internal/interest_topics/infraestructure/selectTopicHandler_test.go
@@ -0,0 +1,101 @@
+package infraestructure
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/unmsmfisi-socialapplication/social_app/internal/interest_topics/application"
+)
+
+type mockInterestTopicUsecase struct {
+	SetInterestTopicsFn func(user_id, interest_id string) error
+}
+
+func (m *mockInterestTopicUsecase) SetInterestTopics(user_id, interest_id string) error {
+	return m.SetInterestTopicsFn(user_id, interest_id)
+}
+func TestHandleHandleSelectTopic(t *testing.T) {
+
+	tests := []struct {
+		name       string
+		inputBody  string
+		mock       func(user_id, interest_id string) error
+		wantStatus int
+		wantBody   string
+	}{
+
+		{
+			name:       "Insertion Failed - user_id",
+			inputBody:  `{"user_id":"123123","interest_id":["1","3","4"]}`,
+			mock:       func(user_id, interest_id string) error { return errors.New("invalid insertion") },
+			wantStatus: http.StatusNotFound,
+			wantBody:   `{"response":"Error during insertion","status":"NOTFOUND"}`,
+		},
+		{
+			name:       "Insertion Failed - interest_id",
+			inputBody:  `{"user_id":"1","interest_id":["12","23","24"]}`,
+			mock:       func(user_id, interest_id string) error { return errors.New("invalid insertion") },
+			wantStatus: http.StatusNotFound,
+			wantBody:   `{"response":"Error during insertion","status":"NOTFOUND"}`,
+		},
+		{
+			name:       "Existing user interest topic",
+			inputBody:  `{"user_id":"1","interest_id":["1","3","4"]}`,
+			mock:       func(user_id, interest_id string) error { return application.ErrInvalidInsertion },
+			wantStatus: http.StatusInternalServerError,
+			wantBody:   `{"response":"Attempted insertion of an existing user interest topic","status":"ERROR"}`,
+		},
+		{
+			name:       "Bad request",
+			inputBody:  `{"user_id": "1","interest_id": ""}`,
+			mock:       func(user_id, interest_id string) error { return nil },
+			wantStatus: http.StatusBadRequest,
+			wantBody:   `{"response":"Invalid request payload","status":"ERROR"}`,
+		},
+
+		{
+			name:       "Insertion successful",
+			inputBody:  `{"user_id":"2","interest_id":["1","3","4"]}`,
+			mock:       func(user_id, interest_id string) error { return nil },
+			wantStatus: http.StatusOK,
+			wantBody:   `{"response":"Insertion successful","status":"OK"}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodPost, "/interestTopics", bytes.NewBufferString(tt.inputBody))
+			if err != nil {
+				t.Fatalf("could not create request: %v", err)
+			}
+
+			mockUseCase := &mockInterestTopicUsecase{
+				SetInterestTopicsFn: tt.mock,
+			}
+			handler := NewSelectTopicHandler(mockUseCase)
+			recorder := httptest.NewRecorder()
+
+			handler.HandleSelectTopic(recorder, req)
+
+			res := recorder.Result()
+			defer res.Body.Close()
+
+			if res.StatusCode != tt.wantStatus {
+				t.Errorf("expected status %v; got %v", tt.wantStatus, res.StatusCode)
+			}
+
+			body, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatalf("could not read response body: %v", err)
+			}
+
+			if string(body) != tt.wantBody {
+				t.Errorf("expected body %q; got %q", tt.wantBody, body)
+			}
+		})
+	}
+}
