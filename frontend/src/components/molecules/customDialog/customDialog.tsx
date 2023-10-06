9be12b8e0a5e1ac5a0cadf7a9bd6799e314cd1af
Jonathan Lara <jonathan.lara@unmsm.edu.pe>
2023-10-06 16:53:41 -0500

    [FE] Fix: abstracted styles

--- frontend/src/components/molecules/customDialog/customDialog.tsx
+++ frontend/src/components/molecules/customDialog/customDialog.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
+import { Theme, styled } from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
@@ -12,10 +12,10 @@ import WButton, { WButtonProps } from "@/components/atoms/Button/button";
 
 
 export interface CustomDialogProps {
-  title: CustomText,
-  content: CustomText,
+  title?: CustomText,
+  content?: CustomText,
   subtitle?: CustomText,
-  size: "xs" | "sm" | "md" | "lg" | "xl",
+  size?: "xs" | "sm" | "md" | "lg" | "xl",
   buttonProps?: WButtonProps
 }
 
@@ -24,19 +24,28 @@ export interface CustomText {
   typeColor?: string;
 }
 
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiDialogContent-root': {
-    padding: theme.spacing(2),
-  },
-  '& .MuiDialogActions-root': {
-    padding: theme.spacing(1),
-  },
+const BootstrapDialog = styled(Dialog)(() => ({
   // agregamos mas border radius para que se vea mas redondo
   '& .MuiPaper-root': {
     borderRadius: '20px',
   },
 }));
 
+const sxDialogTitle = {
+  m: 0,
+  p: 2,
+  textAlign: 'center',
+  fontSize: 20,
+  fontWeight: 700,
+  paddingX: '25px'
+}
+
+const sxCloseButton = {
+  position: 'absolute',
+  right: 8,
+  top: 8,
+  color: (theme: Theme) => theme.palette.grey[500],
+}
 
 const CustomDialog: React.FC<CustomDialogProps> = ({ content, title, subtitle, size, buttonProps }) => {
   const [open, setOpen] = React.useState(false);
@@ -55,47 +64,32 @@ const CustomDialog: React.FC<CustomDialogProps> = ({ content, title, subtitle, s
       </Button>
       <BootstrapDialog
         onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
         open={open}
         maxWidth={size}
         fullWidth={true}
       >
         <DialogTitle
-          sx={{
-            m: 0,
-            p: 2,
-            textAlign: 'center',
-            fontSize: 20,
-            fontWeight: 700,
-            paddingX: '25px' // Espacio adicional para el botón de cerrar
-          }}
-          id="customized-dialog-title"
-          color={title.typeColor}
+          sx={sxDialogTitle}
+          color={title?.typeColor}
         >
-          {title.textContent}
+          {title?.textContent}
         </DialogTitle>
 
         <IconButton
-          aria-label="close"
           onClick={handleClose}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
+          sx={sxCloseButton}
         >
           <CloseIcon />
         </IconButton>
 
         <DialogContent dividers>
           {subtitle && (
-            <Typography sx={{ whiteSpace: 'pre-line', textAlign: 'center', paddingX: 3, fontWeight: 700, fontSize: 16, color: subtitle.typeColor }} gutterBottom>
+            <Typography sx={{textAlign: 'center', fontWeight: 700, fontSize: 16, color: subtitle.typeColor }} gutterBottom>
               {subtitle.textContent}
             </Typography>
           )}
-          <Typography sx={{ whiteSpace: 'pre-line', textAlign: 'center', paddingX: 3, fontWeight: 400, fontSize: 14, color: content.typeColor }} gutterBottom>
-            {content.textContent}
+          <Typography sx={{ whiteSpace: 'pre-line', textAlign: 'center', paddingX: 3, fontWeight: 400, fontSize: 14, color: content?.typeColor }} gutterBottom>
+            {content?.textContent}
           </Typography>
         </DialogContent>
         {buttonProps && (
@@ -107,6 +101,25 @@ const CustomDialog: React.FC<CustomDialogProps> = ({ content, title, subtitle, s
       </BootstrapDialog>
     </div>
   );
+}
+
+CustomDialog.defaultProps = {
+    title: {
+      textContent: "!La foto no cumple con las especificaciones, subir otra foto¡",
+      typeColor: "error",
+    },
+    content: {
+      textContent: "Peso max (10 mb)\nTamaño max (851x315 px)\nLa plataforma acepta estas especificaciones para la foto de portada.",
+    },
+    subtitle: {
+      textContent: "Especificaciones de foto de portada",
+    },
+    size: "xs",
+    buttonProps: {
+      text: "Cargar Foto",
+      typeColor: "primary",
+      size: "large",
+    }
 }
 
 export default CustomDialog;
