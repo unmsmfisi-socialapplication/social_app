f7fe6dad84e9ce79cb0fcb65988066cfb4a5920a
MarcoPorrasQuispe <marco.porras@unmsm.edu.pe>
2023-10-05 21:39:14 -0500

    I did changes instead the old version of ButtonPost

--- frontend/src/components/atoms/ButtonPost/ButtonPost.tsx
+++ frontend/src/components/atoms/ButtonPost/ButtonPost.tsx
@@ -3,36 +3,28 @@ import React from "react";
 import Button from "@mui/material/Button";
 import "./index.scss";
 
-interface WButtonPostProps {
-    typeColor?: "primary" | "secondary" | "disabled";
-    text?: string;
-    size?: "large";
-    disabled?: boolean;
-    onChange?: (event: React.ChangeEvent<HTMLButtonElement>) => void;
+interface WButtonPost {
+  typeColor?: "primary" | "secondary" | "disabled";
+  text?: string;
+  size?: "large";
+  disabled?: boolean;
+  borderRadius?: string;
 }
 
-const WButtonPost: React.FC<WButtonPostProps> = ({ disabled, typeColor, text, size, onChange }) => {
-    const buttonClass = `button typeButton--${disabled ? "disabled" : typeColor}`;
-  
-    const handleChange = (event: React.ChangeEvent<HTMLButtonElement>) => {
-      if (onChange) {
-        onChange(event);
-      }
-    };
-  
-    return (
-      <Button
-        style={{ minWidth: size === "large" ? "100%" : "auto" }}
-        className={buttonClass}
-        size={size}
-        disabled={disabled}
-        onChange={handleChange}
-      >
-        {text}
-      </Button>
-    );
-  };
-  
+
+const WButtonPost: React.FC<WButtonPost> = ({ disabled, typeColor, text, size }) => {
+  const buttonClass = `button typeButton--${disabled ? "disabled" : typeColor}`;
+  return (
+    <Button
+      style={{ minWidth: size === "large" ? "100%" : "auto" }}
+      className={buttonClass}
+      size={size}
+      disabled={disabled}
+    >
+      {text}
+    </Button>
+  );
+};
 
 export default WButtonPost;
 
