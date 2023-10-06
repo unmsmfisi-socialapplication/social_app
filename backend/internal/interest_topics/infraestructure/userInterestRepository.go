70618fc12a8d40173758084e8ad03e5fb4b19611
Angel Carmen Cruzatti <angelcarmen0302@gmail.com>
2023-10-05 19:15:11 -0500

    [Backend] refactor: User interest topics mock setup

--- backend/internal/interest_topics/infraestructure/userInterestRepository.go
+++ backend/internal/interest_topics/infraestructure/userInterestRepository.go
@@ -2,8 +2,11 @@ package infraestructure
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/unmsmfisi-socialapplication/social_app/internal/interest_topics/domain"
+
+	"github.com/unmsmfisi-socialapplication/social_app/internal/interest_topics/application"
 )
 
 type UserInterestsDBRepository struct {
@@ -15,12 +18,48 @@ func NewUserInterestsDBRepository(db *sql.DB) domain.UserInterestsRepository {
 }
 
 func (ui *UserInterestsDBRepository) Create(interest *domain.UserInterestTopics) error {
-	query := `INSERT INTO soc_app_users_interest_topics (user_id, interest_id) VALUES ($1,$2)`
 
-	_, err := ui.db.Exec(query, interest.User_id, interest.Interest_id)
-	if err != nil {
-		return err
+	userIDValues := []string{"1", "2"}
+	interestsIDValues := []string{"1", "2", "3", "4", "5", "6", "7"}
+	userInterestTopics := [][]string{{"1", "2"}, {"1", "3"}, {"1", "4"}}
+
+	if !contains(userIDValues, interest.User_id) || !contains(interestsIDValues, interest.Interest_id) {
+		return errors.New("invalid insertion")
 	}
-
+	if containsSlice(userInterestTopics, []string{interest.User_id, interest.Interest_id}) {
+		return application.ErrInvalidInsertion
+	}
 	return nil
+}
+
+// Temporary function for searching in a one dimensional array
+func contains(arr []string, val string) bool {
+	for _, item := range arr {
+		if item == val {
+			return true
+		}
+	}
+	return false
+}
+
+// Temporary function for searching in a bidimensional array
+func containsSlice(arr2D [][]string, arrayToFind []string) bool {
+	for _, slice := range arr2D {
+		if len(slice) != len(arrayToFind) {
+			continue
+		}
+
+		match := true
+		for i := range slice {
+			if slice[i] != arrayToFind[i] {
+				match = false
+				break
+			}
+		}
+
+		if match {
+			return true
+		}
+	}
+	return false
 }
