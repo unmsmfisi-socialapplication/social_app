e3896091a058ebf9c71cc1ed0e3383e1ef9bc1fe
Patrick <85466884+Patrickpisana23@users.noreply.github.com>
2023-10-06 01:28:05 -0500

    Update file2

--- data/cloud_platorm/GoogleDrive.py
+++ data/cloud_platorm/GoogleDrive.py
@@ -1,11 +1,11 @@
-#from pydrive2.auth import GoogleAuth
-#from pydrive2.drive import GoogleDrive
-#from pydrive2.files import FileNotUploadedError
-#import unittest
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive 
+
+import unittest
 
 directorio_credenciales = 'credentials_module.json'
 
-# log in
+# Login
 def login():
     GoogleAuth.DEFAULT_SETTINGS['client_config_file'] = directorio_credenciales
     gauth = GoogleAuth()
@@ -26,9 +26,9 @@ def login():
 def crear_archivo_texto(nombre_archivo,contenido,id_folder):
     credenciales = login()
     archivo = credenciales.CreateFile({'title': nombre_archivo,\
-                                       'parents': [{"kind": "drive#fileLink",\
-                                                    "id": id_folder}]})
-    archivo.SetContentString('Hey MoonCoders!')
+                                        'parents': [{"kind": "drive#fileLink",\
+                                        "id": id_folder}]})
+    archivo.SetContentString('comment')
     archivo.Upload()
 
 
@@ -43,24 +43,22 @@ def subir_archivo(ruta_archivo,id_folder):
 
 
 def test_subir_archivo(self):
-    #Define a local file path for the test
-    ruta_archivo = 'ruta_de_prueba/fondo_prueba.jpg'   
-
-    #Define a test folder ID in Google Drive
-    id_folder = 'ID_DE_LA_CARPETA_DE_PRUEBA'   
-
-    #Execute the subir_archivo function with the test parameters
+    # Define a local file path for the test
+    ruta_archivo = 'route_file'   
+     # Define a test folder ID in Google Drive
+    id_folder = 'id_folder'   
+    # Execute the subir_archivo function with the test parameters
     subir_archivo(ruta_archivo, id_folder)
 
          
-
-
+ 
 if __name__ == "__main__":
-    #ruta_archivo = '/home/falv/Escritorio/fondo.jpg'
-    #id_folder = '0AI_9cD6f9EEZUk9PVA'
-    #id_drive = '1LVdc-DUwr30kfrA30cVO3K92RVh56pmw'
-    #ruta_descarga = '/home/falv/Descargas/'
-    #crear_archivo_texto('HolaDrive.txt','Hey MoonCoders',id_folder)
-    #subir_archivo(ruta_archivo,id_folder)
-    #unittest.main()
+    ruta_archivo = 'route_file'
+    id_folder = 'id_folder'
+    id_drive = 'id_drive'
+    ruta_descarga = 'download_path'
+    #crear_archivo_texto('file','commet',id_folder)
+    subir_archivo(ruta_archivo,id_folder)
+    unittest.main()
+    
     
