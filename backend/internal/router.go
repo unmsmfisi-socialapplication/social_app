5ba450f5018dfedbe028eeed19f031e1c0e3db3b
Angel Carmen Cruzatti <angelcarmen0302@gmail.com>
2023-10-06 23:08:51 -0500

    [Backend] refactor: Add "/interestTopics" endpoint to router.go

--- backend/internal/router.go
+++ backend/internal/router.go
@@ -13,6 +13,8 @@ import (
 	"github.com/go-chi/cors"
 	"github.com/unmsmfisi-socialapplication/social_app/internal/comment"
 	email "github.com/unmsmfisi-socialapplication/social_app/internal/email_sender"
+	interest_topics_application "github.com/unmsmfisi-socialapplication/social_app/internal/interest_topics/application"
+	interest_topics_infraestructure "github.com/unmsmfisi-socialapplication/social_app/internal/interest_topics/infraestructure"
 	"github.com/unmsmfisi-socialapplication/social_app/internal/login/application"
 	"github.com/unmsmfisi-socialapplication/social_app/internal/login/infrastructure"
 
@@ -50,6 +52,11 @@ func Router() http.Handler {
 	loginUseCase := application.NewLoginUseCase(dbRepo)
 	loginHandler := infrastructure.NewLoginHandler(loginUseCase)
 
+	//interesTopics
+	dbinterestTopics := interest_topics_infraestructure.NewUserInterestsDBRepository(dbInstance)
+	selectTopicUseCase := interest_topics_application.NewInterestTopicsUseCase(dbinterestTopics)
+	selecTopicHandler := interest_topics_infraestructure.NewSelectTopicHandler(selectTopicUseCase)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("{\"hello\": \"world\"}"))
 	})
@@ -74,5 +81,9 @@ func Router() http.Handler {
 
 	emailRouter := email.EmailModuleRouter()
 	r.Mount("/email", emailRouter)
+
+	// Login
+	r.Post("/login", loginHandler.HandleLogin)
+	r.Post("/interestTopics", selecTopicHandler.HandleSelectTopic)
 	return r
 }
