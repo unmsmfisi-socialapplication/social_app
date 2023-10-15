159bf0cb363585a660a5997b0c3151fcc498f702
Alexander Torre <alexander.torre.arteaga@gmail.com>
2023-10-15 16:23:10 -0500

    BE_FIX_Add_Config_Cors_To_.Env

--- backend/test/config_test.go
+++ backend/test/config_test.go
@@ -38,19 +38,14 @@ func TestCheckEnvVariables(t *testing.T) {
 	os.Setenv("DB_HOST", "localhost")
 	os.Setenv("DB_USER", "testuser")
 	os.Setenv("DB_PASSWORD", "testpass")
-	os.Setenv("DB_DBNAME", "testdb")
+	os.Setenv("DB_NAME", "testdb")
 	os.Setenv("DB_SSLMODE", "disable")
 	os.Setenv("DB_SCHEMA", "sa")
+	os.Setenv("CORS_ORIGINS", "[http://localhost:3000,https://social-app-frontend-service-7bg5siys2q-uc.a.run.app/]")
+	os.Setenv("CORS_MAXAGE", "300")
 
 	err := config.CheckEnvVariables()
 	if err != nil {
 		t.Errorf("Se esperaba que todas las variables de entorno estuvieran configuradas, pero se obtuvo un error: %v", err)
-	}
-
-	os.Unsetenv("DB_HOST")
-
-	err = config.CheckEnvVariables()
-	if err == nil {
-		t.Error("Se esperaba un error ya que falta una variable de entorno requerida, pero no se obtuvo ningún error")
 	}
 }
