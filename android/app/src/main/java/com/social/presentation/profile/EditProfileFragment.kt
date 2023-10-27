9ee3a5d76af0be94675a1edd0222ff317727fd0b
Crhistian Villantoy <crhistian.villantoy@hotmail.com>
2023-10-27 14:41:09 -0500

    advance screen edit biography

--- android/app/src/main/java/com/social/presentation/profile/EditProfileFragment.kt
+++ android/app/src/main/java/com/social/presentation/profile/EditProfileFragment.kt
@@ -1,6 +1,8 @@
 package com.social.presentation.profile
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.social.R
@@ -8,16 +10,50 @@ import com.social.databinding.FragmentEditProfileBinding
 
 
 class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
-   private lateinit var binding: FragmentEditProfileBinding
+    private lateinit var binding: FragmentEditProfileBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentEditProfileBinding.bind(view)
-
         action()
     }
 
     private fun action() {
+        userVerification()
+    }
 
+    private fun userVerification() {
+        binding.inputUserName.addTextChangedListener(
+            object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {}
+
+                override fun onTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    before: Int,
+                    count: Int
+                ) {
+                    if (userUnique(s.toString())) {
+                        binding.errorUsername.visibility = View.GONE
+                    } else {
+                        binding.errorUsername.visibility = View.VISIBLE
+                    }
+                }
+                override fun afterTextChanged(s: Editable?) {}
+            }
+        )
+    }
+
+    private fun userUnique(username: String): Boolean {
+        //consulta a backend
+        if (username == "c" || username == "cr" || username == "crh") {
+            return false
+        }
+        return true
     }
 }
