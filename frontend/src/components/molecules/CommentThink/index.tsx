f0c6fa66595f042b60925557b89d4eabad027096
Ricse Anthony <anthony.ricse@unmsm.edu.pe>
2023-10-27 10:30:20 -0500

    fix (eslint) : script format

--- frontend/src/components/molecules/CommentThink/index.tsx
+++ frontend/src/components/molecules/CommentThink/index.tsx
@@ -1,47 +1,39 @@
-"use client";
-import React from "react";
-import "./index.scss";
-import { WButton } from "@/components";
-import Textarea from "@mui/joy/Textarea";
-import WCircleImage from "@/components/atoms/CircleImage/circleImage";
+'use client'
+import React from 'react'
+import './index.scss'
+import { WButton } from '@/components'
+import Textarea from '@mui/joy/Textarea'
+import WCircleImage from '@/components/atoms/CircleImage/circleImage'
 
 interface CommentThinkProps {
-  avatarDefaultURL?: string;
-  publicTag: string;
-  placeholder: string;
+    avatarDefaultURL?: string
+    publicTag: string
+    placeholder: string
 }
 
-const CommentThink: React.FC<CommentThinkProps> = ({
-  avatarDefaultURL,
-  publicTag,
-  placeholder,
-}) => {
-  return (
-    <div className="comment_think_main_container">
-      <div className="comment_think_container">
-        <WCircleImage
-          avatarDefaultURL={avatarDefaultURL}
-          size={80}
-          typeColor="third"
-        />
-        <WButton typeColor="white" text={publicTag} variant="outlined" />
-      </div>
-      <Textarea
-        style={{ height: "200px" }}
-        color="neutral"
-        minRows={2}
-        placeholder={placeholder}
-        size="lg"
-        variant="plain"
-      />
-    </div>
-  );
-};
+const CommentThink: React.FC<CommentThinkProps> = ({ avatarDefaultURL, publicTag, placeholder }) => {
+    return (
+        <div className="comment_think_main_container">
+            <div className="comment_think_container">
+                <WCircleImage avatarDefaultURL={avatarDefaultURL} size={80} typeColor="third" />
+                <WButton typeColor="white" text={publicTag} variant="outlined" />
+            </div>
+            <Textarea
+                style={{ height: '200px' }}
+                color="neutral"
+                minRows={2}
+                placeholder={placeholder}
+                size="lg"
+                variant="plain"
+            />
+        </div>
+    )
+}
 
-export default CommentThink;
+export default CommentThink
 
 CommentThink.defaultProps = {
-  avatarDefaultURL: "",
-  publicTag: "Público",
-  placeholder: "",
-};
+    avatarDefaultURL: '',
+    publicTag: 'Público',
+    placeholder: '',
+}
--- frontend/src/domain/usecases/AuthServises.tsx
+++ frontend/src/domain/usecases/AuthServises.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable prettier/prettier */
-import AuthRepository from '../repositories/AuthRepository';
+import AuthRepository from '../repositories/AuthRepository'
 
 export default class AuthServices {
-
-    static async authRequest(request : any) {
-        const { data , error } = await AuthRepository.authRequest(request);
-        return { data , error }
+    static async authRequest(request: any) {
+        const { data, error } = await AuthRepository.authRequest(request)
+        return { data, error }
     }
 }
