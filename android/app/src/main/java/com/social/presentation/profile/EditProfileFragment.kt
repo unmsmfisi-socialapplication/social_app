54ec06007be194050d04448d86d99d52be5cc1ef
Crhistian Villantoy <crhistian.villantoy@hotmail.com>
2023-10-27 16:25:34 -0500

    edit biography: upload profile picture

--- android/app/src/main/java/com/social/presentation/profile/EditProfileFragment.kt
+++ android/app/src/main/java/com/social/presentation/profile/EditProfileFragment.kt
@@ -1,16 +1,28 @@
 package com.social.presentation.profile
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.social.R
 import com.social.databinding.FragmentEditProfileBinding
 
-
 class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
     private lateinit var binding: FragmentEditProfileBinding
+    private val imagePicker =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK && result.data != null) {
+                result.data?.data?.let {
+                    uploadImage(it)
+                }
+            }
+        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -19,7 +31,23 @@ class EditProfileFragment : Fragment(R.layout.fragment_edit_profile) {
     }
 
     private fun action() {
+        binding.buttonUploadProfile.setOnClickListener {
+            openGallery()
+        }
         userVerification()
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        imagePicker.launch(intent)
+    }
+
+    private fun uploadImage(uri: Uri) {
+        val bitmap = BitmapFactory.decodeStream(
+            requireContext().contentResolver.openInputStream(uri)
+        )
+        binding.imagenProfile.setImageBitmap(bitmap)
     }
 
     private fun userVerification() {
