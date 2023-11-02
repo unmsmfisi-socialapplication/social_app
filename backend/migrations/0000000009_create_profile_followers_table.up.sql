13cb7df1089e29e88f401442739b421ae1ce6c52
francosilva25 <e.franco250299@gmail.com>
2023-11-02 15:17:51 -0500

    [Backend] feat: Add Follow Profiles Tables

---
+++ backend/migrations/0000000009_create_profile_followers_table.up.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+CREATE TABLE sa.SOC_APP_USER_PROFILE_FOLLOW (
+  follow_id bigserial,
+  follower_user_id bigint NOT NULL,
+  following_user_id bigint NOT NULL,
+  follow_date timestamp,
+  PRIMARY KEY (follow_id),
+  FOREIGN KEY (follower_user_id) REFERENCES SOC_APP_USER_PROFILE(profile_id),
+  FOREIGN KEY (following_user_id) REFERENCES SOC_APP_USER_PROFILE(profile_id)
+);
+CREATE UNIQUE INDEX UQ_FOLLOW_DATE ON sa.SOC_APP_USER_PROFILE_FOLLOW(follower_user_id,following_user_id);
+CREATE INDEX IDX_FOLLOW_DATE ON sa.SOC_APP_USER_PROFILE_FOLLOW(follow_date);
+
+COMMIT;
