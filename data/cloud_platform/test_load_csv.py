0f56a78d3cde4ed4e1be0610f32f4b3b07985cb0
Patrickpisana23 <patrickpisana@unmsm.edu.pe>
2023-11-03 17:46:57 -0500

    New parameters for testing

--- data/cloud_platform/test_load_csv.py
+++ data/cloud_platform/test_load_csv.py
@@ -1,7 +1,18 @@
+import pytest
 from load_csv import consume_api_and_save_csv
  
 
-def test_load():
-    assert consume_api_and_save_csv("https://jsonplaceholder.typicode.com/posts") == True
-
  
+
+@pytest.mark.parametrize(
+    "input_url, expected",
+    [
+        ("https://jsonplaceholder.typicode.com/posts",True),
+        ("https://jsonplaceholder.typicode.com/users", True),
+        ("https://jsonplaceholder.typicode.com/photos", True)
+       
+    ]
+)
+
+def test_consume_api(input_url,expected):
+    assert consume_api_and_save_csv(input_url) == True
