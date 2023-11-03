345f68c3f1655e7d6d8991459d83cbab5c584b2a
TharlezMontalvo <75101071+TharlezMontalvo@users.noreply.github.com>
2023-11-03 18:26:09 -0500

    I did the follow person card component

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -124,7 +124,7 @@ export default function TestPage() {
                 <WCardFollow
                     avatar="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                     name="XokasXD"
-                    handle="XokasXD"
+                    userhandle="XokasXD"
                 />
             </div>
         </Layout>
