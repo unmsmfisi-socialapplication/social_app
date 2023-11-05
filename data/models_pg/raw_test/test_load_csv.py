a9f4bedd1015f74d260457b42815982d924c28db
Patrickpisana23 <patrickpisana@unmsm.edu.pe>
2023-11-05 02:26:14 -0500

    New parameters for testing

---
+++ data/models_pg/raw_test/test_load_csv.py
@@ -0,0 +1,13 @@
+import pytest
+from  raw.raw_collection import consume_api_and_save_csv
+
+@pytest.mark.parametrize(
+    "input_url, expected",
+    [
+        ("https://jsonplaceholder.typicode.com/posts", True),
+        ("https://jsonplaceholder.typicode.com/users", True),
+        ("https://jsonplaceholder.typicode.com/photos", True)
+    ]
+)
+def test_consume_api(input_url, expected):
+    assert consume_api_and_save_csv(input_url) 
