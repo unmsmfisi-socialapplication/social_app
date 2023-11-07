52a64a4eb99574f72bae4ec028b3b69730442072
Franco Silva <67833630+francosilva25@users.noreply.github.com>
2023-11-06 22:31:25 -0500

    [Backend] fix: Schema for Up Follow Profile Table

--- backend/migrations/0000000009_create_profile_followers_table.up.sql
+++ backend/migrations/0000000009_create_profile_followers_table.up.sql
@@ -6,10 +6,10 @@ CREATE TABLE sa.SOC_APP_USER_PROFILE_FOLLOW (
   following_profile_id bigint NOT NULL,
   follow_date timestamp,
   PRIMARY KEY (follow_id),
-  FOREIGN KEY (follower_profile_id) REFERENCES sa.SOC_APP_USER_PROFILE(profile_id),
-  FOREIGN KEY (following_profile_id) REFERENCES sa.SOC_APP_USER_PROFILE(profile_id)
+  FOREIGN KEY (follower_profile_id) REFERENCES SOC_APP_USER_PROFILE(profile_id),
+  FOREIGN KEY (following_profile_id) REFERENCES SOC_APP_USER_PROFILE(profile_id)
 );
-CREATE UNIQUE INDEX UQ_SOC_APP_USER_PROFILE_FOLLOW ON sa.SOC_APP_USER_PROFILE_FOLLOW(follower_profile_id,following_profile_id);
-CREATE INDEX IDX_FOLLOW_DATE ON sa.SOC_APP_USER_PROFILE_FOLLOW(follow_date);
+CREATE UNIQUE INDEX UQ_SOC_APP_USER_PROFILE_FOLLOW ON SOC_APP_USER_PROFILE_FOLLOW(follower_profile_id,following_profile_id);
+CREATE INDEX IDX_FOLLOW_DATE ON SOC_APP_USER_PROFILE_FOLLOW(follow_date);
 
 COMMIT;
