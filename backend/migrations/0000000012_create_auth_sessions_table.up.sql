1782e55d16e7daf068f455d28c0d968c7c4d413a
julianntorres <67835221+julianntorres@users.noreply.github.com>
2023-11-08 18:23:54 -0500

    Revert "[BE] Addition of soc_app_auth_sessions Table to Handle User Sessions"

--- backend/migrations/0000000012_create_auth_sessions_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE soc_app_auth_sessions (
-    id integer NOT NULL,
-    user_name character varying(255) NOT NULL,
-    logged boolean,
-    "timestamp" timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    jti character varying(255)
-);
