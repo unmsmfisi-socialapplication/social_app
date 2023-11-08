74a44ff6df4f11dc8349de7a48cbac7424468bf7
Crhistian Villantoy <crhistian.villantoy@hotmail.com>
2023-11-08 13:40:22 -0500

    correction lint build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -103,7 +103,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
-    testImplementation ("org.mockito:mockito-core:5.1.1")
+    testImplementation("org.mockito:mockito-core:5.1.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.01"))
