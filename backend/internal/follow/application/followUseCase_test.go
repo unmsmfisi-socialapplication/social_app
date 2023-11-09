fb6f8acea2a1b5b7d0de1fb70642401cd26f5780
francosilva25 <e.franco250299@gmail.com>
2023-11-08 20:06:20 -0500

    [Backend] test: Add test for followUseCase.go

--- backend/internal/follow/application/followUseCase_test.go
+++ backend/internal/follow/application/followUseCase_test.go
@@ -1,55 +1,63 @@
 package application
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/unmsmfisi-socialapplication/social_app/internal/follow/domain"
 )
 
-type MockRepo struct {
-	insertedFollower *domain.Follower
-	insertError      error
+// Mock para el repositorio
+type MockFollowerRepository struct{}
+
+func (m *MockFollowerRepository) InsertNewFollower(newFollower *domain.Follower) (*domain.Follower, error) {
+	return &domain.Follower{}, nil
 }
 
-func (m *MockRepo) InsertNewFollower(follower *domain.Follower) (*domain.Follower, error) {
-	return m.insertedFollower, m.insertError
+func (m *MockFollowerRepository) IsFollowing(newFollower *domain.Follower) (*bool, error) {
+	return new(bool), nil
+}
+
+func TestNewFollowerUseCase(t *testing.T) {
+	// Crear un Mock del repositorio
+	repo := &MockFollowerRepository{}
+	useCase := NewFollowerUseCase(repo)
+
+	// Ejecutar la función y asegurarse de que no se produzca un error
+	newFollower, err := useCase.FollowProfile(1, 2)
+	if err != nil {
+		t.Errorf("Error inesperado: %v", err)
+	}
+	if newFollower == nil {
+		t.Error("newFollower es nulo, pero se esperaba un valor no nulo")
+	}
 }
 
 func TestFollowProfile(t *testing.T) {
-	mockRepo := &MockRepo{}
+	// Crear un Mock del repositorio
+	repo := &MockFollowerRepository{}
+	useCase := NewFollowerUseCase(repo)
 
-	useCase := NewFollowerUseCase(mockRepo)
+	// Ejecutar la función y asegurarse de que no se produzca un error
+	newFollower, err := useCase.FollowProfile(1, 2)
+	if err != nil {
+		t.Errorf("Error inesperado: %v", err)
+	}
+	if newFollower == nil {
+		t.Error("newFollower es nulo, pero se esperaba un valor no nulo")
+	}
+}
 
-	t.Run("Test con éxito", func(t *testing.T) {
-		mockRepo.insertedFollower = &domain.Follower{Follower_profile_id: 1, Following_profile_id: 2}
+func TestIsFollowing(t *testing.T) {
+	// Crear un Mock del repositorio
+	repo := &MockFollowerRepository{}
+	useCase := NewFollowerUseCase(repo)
 
-		follower, err := useCase.FollowProfile(1, 2)
-
-		if err != nil {
-			t.Errorf("Se esperaba un error nulo, pero se obtuvo: %v", err)
-		}
-
-		if follower == nil {
-			t.Error("Se esperaba un seguidor, pero se obtuvo nulo")
-		}
-
-		if follower.Follower_profile_id != 1 || follower.Following_profile_id != 2 {
-			t.Errorf("El seguidor devuelto no coincide con las expectativas: %v", follower)
-		}
-	})
-
-	t.Run("Test con error al insertar", func(t *testing.T) {
-		mockRepo.insertError = errors.New("Error al insertar")
-
-		follower, err := useCase.FollowProfile(1, 2)
-
-		if err == nil {
-			t.Error("Se esperaba un error, pero se obtuvo un error nulo")
-		}
-
-		if follower != nil {
-			t.Error("Se esperaba un seguidor nulo, pero se obtuvo un seguidor")
-		}
-	})
+	// Ejecutar la función y asegurarse de que no se produzca un error
+	isFollowing, err := useCase.IsFollowing(1, 2)
+	if err != nil {
+		t.Errorf("Error inesperado: %v", err)
+	}
+	if isFollowing == nil {
+		t.Error("isFollowing es nulo, pero se esperaba un valor no nulo")
+	}
 }
