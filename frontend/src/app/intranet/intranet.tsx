1edecdc3b9d36dadcc32c0ddf20c9649a7dde857
TharlezMontalvo <75101071+TharlezMontalvo@users.noreply.github.com>
2023-11-09 15:34:03 -0500

    I put the list option in side bar component

--- frontend/src/app/intranet/intranet.tsx
+++ frontend/src/app/intranet/intranet.tsx
@@ -3,18 +3,10 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import RootLayout from '../layout'
 import { Box, Grid, Button } from '@mui/material'
-import { WButton, WTag } from '@/components'
 import './index.scss'
 import SideBar from '@/components/organisms/SideBar/SideBar'
 import RightBar from '@/components/organisms/RightBar/RightBar'
-import SearchIcon from '@mui/icons-material/Search'
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
-import MailOutlineIcon from '@mui/icons-material/MailOutline'
-import CottageOutlinedIcon from '@mui/icons-material/CottageOutlined'
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted'
-import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder'
-import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline'
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
+
 interface PropsIntranet {
     children: React.ReactNode
     rightBar?: boolean
@@ -33,17 +25,6 @@ export default function IntranetHoc({ children, sideBar, rightBar }: PropsIntran
                 <Grid container spacing={{ xs: 2, md: 3 }}>
                     <Grid item xs={3} sm={3} md={3}>
                         {sideBar && <SideBar />}
-                        <Box className="listOption">
-                            <WTag text="Home" icon={CottageOutlinedIcon} isActive />
-                            <WTag text="Explorer" icon={SearchIcon} />
-                            <WTag text="Notifications" icon={NotificationsNoneIcon} />
-                            <WTag text="Messages" icon={MailOutlineIcon} />
-                            <WTag text="Lists" icon={FormatListBulletedIcon} />
-                            <WTag text="Bookmarks" icon={BookmarkBorderIcon} />
-                            <WTag text="Communities" icon={PeopleOutlineIcon} />
-                            <WTag text="Profile" icon={PersonOutlineIcon} />
-                        </Box>
-                        <WButton variant="contained" text="Post" size="large" />
                     </Grid>
                     <Grid item xs={rightBar ? 6 : 8} sm={rightBar ? 6 : 8} md={rightBar ? 6 : 8}>
                         <Box className="home_section">{children}</Box>
--- frontend/src/components/organisms/SideBar/SideBar.tsx
+++ frontend/src/components/organisms/SideBar/SideBar.tsx
@@ -1,14 +1,33 @@
 'use client'
-import { WLogo } from '@/components'
+import { WLogo, WButton, WTag } from '@/components'
 import { Box } from '@mui/material'
 import './index.scss'
+import SearchIcon from '@mui/icons-material/Search'
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone'
+import MailOutlineIcon from '@mui/icons-material/MailOutline'
+import CottageOutlinedIcon from '@mui/icons-material/CottageOutlined'
+import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted'
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder'
+import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline'
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
 interface SideBarProps {}
 
 export default function SideBar({}: SideBarProps) {
     return (
         <Box className="sidebar__section">
             <WLogo />
-            <div>lista de items</div>
+            <div> </div>
+            <Box className="listOption">
+                <WTag text="Home" icon={CottageOutlinedIcon} isActive />
+                <WTag text="Explorer" icon={SearchIcon} />
+                <WTag text="Notifications" icon={NotificationsNoneIcon} />
+                <WTag text="Messages" icon={MailOutlineIcon} />
+                <WTag text="Lists" icon={FormatListBulletedIcon} />
+                <WTag text="Bookmarks" icon={BookmarkBorderIcon} />
+                <WTag text="Communities" icon={PeopleOutlineIcon} />
+                <WTag text="Profile" icon={PersonOutlineIcon} />
+            </Box>
+            <WButton variant="contained" text="Post" />
         </Box>
     )
 }
