e7ebf3e4120dd5dd14362cb43482881e4f309b46
Jeanpiere Julian Palacios Barrutia <87631120+JeanpierePalacios@users.noreply.github.com>
2023-11-09 00:56:15 -0500

    [M] implementation emojis and stickers
    In this task, the emojis and stickers for the chat were created. It should be noted that the "vanniktech:emoji-google" library was used.

--- android/app/src/main/java/com/social/presentation/interaction/chats/ListChatsFragment.kt
+++ android/app/src/main/java/com/social/presentation/interaction/chats/ListChatsFragment.kt
@@ -1,8 +1,59 @@
 package com.social.presentation.interaction.chats
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.social.R
+import com.social.databinding.FragmentChatScreenBinding
+import com.social.databinding.FragmentListChatBinding
+import com.social.utils.Toast
 
 class ListChatsFragment : Fragment(R.layout.fragment_list_chat) {
+    private lateinit var binding: FragmentListChatBinding
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentListChatBinding.bind(view)
+        action()
+    }
+
+    private fun action() {
+        binding.btnBar.setOnClickListener {
+            BarButton()
+        }
+
+        binding.btnListChats.setOnClickListener{
+            ListChatsButton()
+        }
+
+        binding.btnMessages.setOnClickListener{
+            MessageButton()
+        }
+
+        binding.btnMessagesConfig.setOnClickListener {
+            ListChatsConfigButton()
+        }
+    }
+
+    private fun BarButton() {
+        Toast.showMessage(requireContext(), "En desarrollo")
+    }
+
+    private fun ListChatsButton() {
+        Toast.showMessage(requireContext(), "En desarrollo")
+    }
+
+    private fun MessageButton() {
+        Toast.showMessage(requireContext(), "En desarrollo")
+    }
+
+    private fun ListChatsConfigButton() {
+        Toast.showMessage(requireContext(), "En desarrollo")
+    }
+
     // backend
 }
