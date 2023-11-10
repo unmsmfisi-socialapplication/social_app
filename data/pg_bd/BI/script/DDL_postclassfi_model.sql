709811f094ad570dbf112c26fd8fdfa080c6e8e7
EGH2019 <elian.gomez@unmsm.edu.pe>
2023-11-09 23:54:34 -0500

    Bi_postClassification_model dll and sp

---
+++ data/pg_bd/BI/script/DDL_postclassfi_model.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS bi.ResultsPostClasif
+(
+    "ID" INT ,
+    post_description TEXT ,
+    category TEXT ,
+    model_version VARCHAR[20] ,
+    feh_create TIMESTAMP ,
+	name_user VARCHAR[20]
+)
