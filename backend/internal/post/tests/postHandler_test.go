f919d4c87d740f86f73eb463a578547191dd7860
altoma123 <tokkto.jr@hotmail.com>
2023-11-10 18:26:56 -0500

    [Backend] test: Add test cases for TimelineHandler

--- backend/internal/post/tests/postHandler_test.go
+++ backend/internal/post/tests/postHandler_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/unmsmfisi-socialapplication/social_app/internal/post/domain"
 	"github.com/unmsmfisi-socialapplication/social_app/internal/post/infrastructure"
@@ -15,6 +16,8 @@ import (
 type mockPostUseCase struct {
 	CreatePostFn func(post domain.PostCreate) (*domain.Post, error)
 	GetPostsFn   func(params domain.PostPaginationParams) (*domain.PostPagination, error)
+
+	RetrieveTimelinePostsFn func(user_id int64) (*[]domain.TimelineRes, error)
 }
 
 func (m *mockPostUseCase) CreatePost(post domain.PostCreate) (*domain.Post, error) {
@@ -23,6 +26,10 @@ func (m *mockPostUseCase) CreatePost(post domain.PostCreate) (*domain.Post, erro
 
 func (m *mockPostUseCase) GetPosts(params domain.PostPaginationParams) (*domain.PostPagination, error) {
 	return m.GetPostsFn(params)
+}
+
+func (m *mockPostUseCase) RetrieveTimelinePosts(user_id int64) (*[]domain.TimelineRes, error) {
+	return m.RetrieveTimelinePostsFn(user_id)
 }
 
 func TestHandleCreatePost(t *testing.T) {
@@ -146,6 +153,87 @@ func TestHandleGetAllPost(t *testing.T) {
 			res := recorder.Result()
 			defer res.Body.Close()
 
+			if res.StatusCode != tt.wantStatus {
+				t.Errorf("expected status %v; got %v", tt.wantStatus, res.StatusCode)
+			}
+
+			body, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatalf("could not read response: %v", err)
+			}
+
+			if string(body) != tt.wantBody {
+				t.Errorf("expected body %q; got %q", tt.wantBody, body)
+			}
+		})
+	}
+}
+
+func TestHandleTimeline(t *testing.T) {
+	tests := []struct {
+		name         string
+		inputBody    string
+		mockTimeline func(user_id int64) (*[]domain.TimelineRes, error)
+		wantStatus   int
+		wantBody     string
+	}{
+		{
+			name:      "Valid Request",
+			inputBody: `{"user_id": 0}`,
+			mockTimeline: func(user_id int64) (*[]domain.TimelineRes, error) {
+				return &[]domain.TimelineRes{
+					{
+						UserId:        123,
+						PostId:        321,
+						Title:         "Post321",
+						Description:   "Post321 Description",
+						Multimedia:    "",
+						InsertionDate: time.Date(2021, time.Month(2), 21, 1, 10, 30, 0, time.UTC),
+						Username:      "User123",
+					}, {
+						UserId:        456,
+						PostId:        654,
+						Title:         "Post654",
+						Description:   "Post654 Description",
+						Multimedia:    "",
+						InsertionDate: time.Date(2021, time.Month(2), 21, 1, 10, 30, 0, time.UTC),
+						Username:      "User456",
+					}}, nil
+			},
+			wantStatus: http.StatusOK,
+
+			wantBody: `{"response":[{"userId":123,"postId":321,"title":"Post321","description":"Post321 Description","multimedia":"","insertionDate":"2021-02-21T01:10:30Z","username":"User123"},{"userId":456,"postId":654,"title":"Post654","description":"Post654 Description","multimedia":"","insertionDate":"2021-02-21T01:10:30Z","username":"User456"}],"status":"SUCCESS"}`,
+		},
+		{
+			name:      "Internal Server Error",
+			inputBody: `{"user_id": 0}`,
+			mockTimeline: func(user_id int64) (*[]domain.TimelineRes, error) {
+				return nil, fmt.Errorf("Internal Server Error")
+			},
+			wantStatus: http.StatusInternalServerError,
+			wantBody:   `{"response":"Internal Server Error","status":"ERROR"}`,
+		},
+	}
+
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodGet, "/", bytes.NewBufferString(tt.inputBody))
+			fmt.Print(req.Body)
+			fmt.Print(nil)
+			if err != nil {
+				t.Fatalf("could not create request: %v", err)
+			}
+
+			mockUseCase := &mockPostUseCase{
+				RetrieveTimelinePostsFn: tt.mockTimeline,
+			}
+			handler := infrastructure.NewPostHandler(mockUseCase)
+			recorder := httptest.NewRecorder()
+
+			handler.HandleTimeline(recorder, req)
+			res := recorder.Result()
+			defer res.Body.Close()
 			if res.StatusCode != tt.wantStatus {
 				t.Errorf("expected status %v; got %v", tt.wantStatus, res.StatusCode)
 			}
