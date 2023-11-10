d305889cee1eb40ea4c9c9bebf7e72f1b2c40392
Sebas C <sebastian.cano@unmsm.edu.pe>
2023-11-10 11:03:17 -0500

    fix:user_chat_cta_component

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -153,11 +153,7 @@ export default function TestPage() {
                     userhandle="XokasXD"
                 />
             </div>
-            <WUserCHATCTA
-                userName="XokasXD"
-                userHandle="XokasXD"
-                avatarURL="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            />
+            <WUserCHATCTA />
             <div>
                 <WTopicFollow name="Tecnología" topicHandle="Todo sobre tecnología" />
             </div>
--- frontend/src/components/molecules/UserChatCTA/index.tsx
+++ frontend/src/components/molecules/UserChatCTA/index.tsx
@@ -3,12 +3,12 @@ import { Avatar } from '@mui/material'
 import './index.scss'
 
 interface WUserCHATCTAProps {
-    avatarURL: string
-    userName: string
-    userHandle: string
+    avatarURL?: string
+    userName?: string
+    userHandle?: string
 }
 
-const WUserChatCTA = ({ avatarURL = '', userName = 'User', userHandle = 'user' }: WUserCHATCTAProps) => {
+const WUserChatCTA: React.FC<WUserCHATCTAProps> = ({ avatarURL, userName, userHandle }: WUserCHATCTAProps) => {
     const [showCTAOptions, setShowCTAOptions] = useState<boolean>(false)
     return (
         <div className="chat_user_cta_container">
@@ -35,3 +35,10 @@ const WUserChatCTA = ({ avatarURL = '', userName = 'User', userHandle = 'user' }
 }
 
 export default WUserChatCTA
+
+WUserChatCTA.defaultProps = {
+    avatarURL:
+        'https://images.ecestaticos.com/FjaDMYL1rpd8bqAVvR91YL-gZbY=/0x0:2252x1336/1200x1200/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Fae2%2F47e%2F66d%2Fae247e66d9b8d8928d41a592b61690ca.jpg',
+    userName: 'XokasXD',
+    userHandle: 'XokasXD',
+}
