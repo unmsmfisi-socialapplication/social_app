6dfa40e0d02e532451d3d00cf3fd35484eef9404
altoma123 <tokkto.jr@hotmail.com>
2023-11-10 23:46:36 -0500

    [Backend] fix: Add range validation to date inputs

--- backend/internal/comment_dataset/infrastructure/commetDatasetHandler.go
+++ backend/internal/comment_dataset/infrastructure/commetDatasetHandler.go
@@ -1,7 +1,9 @@
 package infrastructure
 
 import (
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/unmsmfisi-socialapplication/social_app/internal/comment_dataset/application"
 	"github.com/unmsmfisi-socialapplication/social_app/pkg/utils"
@@ -19,6 +21,17 @@ func (cdh *commentDatasetHandler) HandleRetrieveScopedComments(w http.ResponseWr
 
 	start_date := r.URL.Query().Get("start_date")
 	end_date := r.URL.Query().Get("end_date")
+
+	sd, err_sd := time.Parse(time.RFC3339, start_date)
+	ed, err_ed := time.Parse(time.RFC3339, end_date)
+	if err_sd != nil || err_ed != nil {
+		log.Fatal("Date Format should be like: 2016-02-02T15:04:05Z")
+		return
+	}
+	if ed.Before(sd) {
+		log.Fatal("Incorrect Date range")
+		return
+	}
 
 	comments, err := cdh.useCase.RetrieveDateScopedComments(start_date, end_date)
 	if err != nil {
