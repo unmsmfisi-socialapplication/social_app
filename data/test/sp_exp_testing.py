bbf02ba8bf521980da115d1f35551a9bc0eb4f30
ZEdgar19 <edgar.zenobio@unmsm.edu.pe>
2023-11-10 22:19:03 -0500

    fix: Correct syxtax error for ci

--- data/test/sp_exp_testing.py
+++ data/test/sp_exp_testing.py
@@ -3,8 +3,6 @@ import requests
 import io
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 import re
