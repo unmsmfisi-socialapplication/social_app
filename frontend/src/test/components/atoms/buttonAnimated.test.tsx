a05d2a54ff1dac4dd2cd00133765042b7157207d
wilfredo huallpartupa <wilfredo.huallpartupa@panbox.io>
2023-11-23 00:14:46 -0500

    add test for component ButtonAnimated

---
+++ frontend/src/test/components/atoms/buttonAnimated.test.tsx
@@ -0,0 +1,15 @@
+import  WButtonMotion  from './../../../components/atoms/ButtonAnimated/buttonAnimated';
+import { render } from '@testing-library/react';
+
+describe('ButtonAnimated', () => {
+    it('renders the ButtonAnimated', () => {
+        const { getByTestId } = render(<WButtonMotion dataTestid="buttonAnimated" text="Go" />)
+
+        expect(getByTestId('buttonAnimated')).toHaveTextContent('Go')
+    })
+    it('renders the disabled ButtonAnimated', () => {
+        const { getByTestId } = render(<WButtonMotion dataTestid="buttonAnimated" text="Go" disabled={true} />)
+
+        expect(getByTestId('buttonAnimated')).toBeDisabled()
+    })
+})
