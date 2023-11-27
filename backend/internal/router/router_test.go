40900cb3b472ee6f13ae3db32776381ffc37770d
Julian Torres <julianse.torres@gmail.com>
2023-11-27 02:47:11 -0500

    [Backend] test: Add Unit Tests for Enhanced Router with TokenValidator Middleware Enhanced the application's router testing suite to validate responses for both authenticated (protected) and public (free) access routes. These tests ensure that our routing logic accurately differentiates between routes requiring user authentication and those accessible to all users. Key Test Scenarios: * 'Protected Route with Authentication header (valid token)': Confirms that the protected route grants access when a valid authentication token is provided. * 'Protected Route without Authentication': Validates that access to the protected route is denied when no authentication token is present, ensuring security. * 'Protected Route with Authentication header (invalid token)': Tests the router's ability to deny access to protected routes with invalid tokens, reinforcing the robustness of our authentication mechanism. * 'Free Route': Checks that public routes are accessible without any authentication token, maintaining ease of access for non-sensitive endpoints.
    These tests are crucial for verifying the effectiveness of our TokenValidator middleware and ensuring that the application's routing adheres to our security protocols.

---
+++ backend/internal/router/router_test.go
@@ -0,0 +1,113 @@
+package router_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-chi/chi"
+	"github.com/unmsmfisi-socialapplication/social_app/internal/router"
+	"github.com/unmsmfisi-socialapplication/social_app/internal/ws/infraestructure"
+)
+
+func mockAuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Header.Get("Authorization") != "" {
+			ctx := r.Context()
+			next.ServeHTTP(w, r.WithContext(ctx))
+		} else {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		}
+	})
+}
+func mockHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message":"success"}`))
+}
+func mockRouter() http.Handler {
+	freeRoutes := chi.NewRouter()
+
+	protectedRoutes := chi.NewRouter()
+	protectedRoutes.Use(mockAuthMiddleware)
+	protectedRoutes.Get("/protected", mockHandler)
+
+	freeRoutes.Get("/free", mockHandler)
+	freeRoutes.Mount("/", protectedRoutes)
+
+	return freeRoutes
+}
+
+func TestProtectedRoutes(t *testing.T) {
+	r := mockRouter()
+	setupTestEnv(t)
+	mockWsHandler := infraestructure.NewHandler(nil)
+	_ = router.Router(mockWsHandler)
+
+	tests := []struct {
+		name       string
+		route      string
+		method     string
+		headers    map[string]string
+		wantStatus int
+	}{
+		{
+			name:       "Protected Route with Authentication header (valid token)",
+			route:      "/protected",
+			method:     http.MethodGet,
+			headers:    map[string]string{"Authorization": "valid_token"},
+			wantStatus: http.StatusOK,
+		},
+		{
+			name:       "Protected Route without Authentication",
+			route:      "/protected",
+			method:     http.MethodGet,
+			headers:    nil,
+			wantStatus: http.StatusUnauthorized,
+		},
+		{
+			name:       "Protected Route with Authentication header (invalid token)",
+			route:      "/protected",
+			method:     http.MethodGet,
+			wantStatus: http.StatusUnauthorized,
+		},
+		{
+			name:       "Free Route",
+			route:      "/free",
+			method:     http.MethodGet,
+			wantStatus: http.StatusOK,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest(tc.method, tc.route, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			for key, value := range tc.headers {
+				req.Header.Set(key, value)
+			}
+
+			rr := httptest.NewRecorder()
+			r.ServeHTTP(rr, req)
+
+			if rr.Code != tc.wantStatus {
+				t.Errorf("%s: expected status code %v, got %v", tc.name, tc.wantStatus, rr.Code)
+			}
+		})
+	}
+}
+
+func setupTestEnv(t *testing.T) {
+	t.Setenv("JWT_SECRET_KEY", "secret")
+	t.Setenv("DB_HOST", "localhost")
+	t.Setenv("DB_USER", "postgres")
+	t.Setenv("DB_PASSWORD", "root")
+	t.Setenv("DB_NAME", "redsocial")
+	t.Setenv("DB_SSLMODE", "disable")
+	t.Setenv("DB_SCHEMA", "sa")
+	t.Setenv("CORS_ORIGINS", "*")
+	t.Setenv("CORS_MAXAGE", "300")
+}
