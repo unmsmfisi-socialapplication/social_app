559a40f72d96dbff55c67252088dab4d01c7666f
Crhistian Villantoy <crhistian.villantoy@hotmail.com>
2023-11-27 18:37:49 -0500

    screen new post

---
+++ android/app/src/main/java/com/social/presentation/home/NewPostFragment.kt
@@ -0,0 +1,27 @@
+package com.social.presentation.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.social.R
+import com.social.databinding.FragmentNewPostBinding
+
+class NewPostFragment : Fragment(R.layout.fragment_new_post) {
+    private lateinit var binding: FragmentNewPostBinding
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentNewPostBinding.bind(view)
+
+        action()
+    }
+
+    private fun action() {
+        binding.iconLocation.setOnClickListener {
+
+        }
+    }
+}
