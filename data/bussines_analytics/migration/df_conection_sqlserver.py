f318a3fa8c892380c36433665d988e5bdaaf1410
Patrickpisana23 <patrickpisana@unmsm.edu.pe>
2023-11-27 23:06:53 -0500

    Conexion sqlserver

--- data/bussines_analytics/migration/df_conection_sqlserver.py
+++ data/bussines_analytics/migration/df_conection_sqlserver.py
@@ -1,7 +1,7 @@
 import pyodbc
 
 
-# Prueba de conexión
+# Connection Testing
 try:
     connection = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};'
                                 'Server=tcp:sv-socialdb-sql.database.windows.net,1433;'
@@ -9,15 +9,15 @@ try:
     print("Conexión exitosa")
 
 
-    # Crear un cursor
+    # Create a cursor
     cursor = connection.cursor()
 
-    # Ejecutar una consulta de prueba
+    # Run a test query
     cursor.execute("SELECT @@version;")
     row = cursor.fetchone()
     print("Versión del servidor de SQL Server: {}".format(row))
 
-    # Ejecutar una consulta en la tabla 'bi_log'
+    # Run a query on the 'bi_log' table
     cursor.execute("SELECT * FROM bi_log")
     rows = cursor.fetchall()
     print(rows)
@@ -28,7 +28,7 @@ except Exception as ex:
     print(f"Error de conexión: {ex}")
 
 finally:
-    # Cerrar la conexión al finalizar
+    # Close the connection at completion
     if connection:
         connection.close()
         print("Conexión cerrada")
