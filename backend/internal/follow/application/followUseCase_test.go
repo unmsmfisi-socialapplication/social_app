833ed818f71db2eee4ba9a59e9f9c0359ec53505
francosilva25 <e.franco250299@gmail.com>
2023-11-27 22:44:58 -0500

    [Backend] test: Adding more cases to the tests to the profile tracking check.

--- backend/internal/follow/application/followUseCase_test.go
+++ backend/internal/follow/application/followUseCase_test.go
@@ -25,10 +25,10 @@ func TestNewFollowerUseCase(t *testing.T) {
 	// Ejecutar la función y asegurarse de que no se produzca un error
 	newFollower, err := useCase.FollowProfile(1, 2)
 	if err != nil {
-		t.Errorf("Error inesperado: %v", err)
+		t.Errorf("Unexpected error: %v", err)
 	}
 	if newFollower == nil {
-		t.Error("newFollower es nulo, pero se esperaba un valor no nulo")
+		t.Error("newFollower is null, but a non-null value was expected.")
 	}
 }
 
@@ -37,13 +37,17 @@ func TestFollowProfile(t *testing.T) {
 	repo := &MockFollowerRepository{}
 	useCase := NewFollowerUseCase(repo)
 
-	// Ejecutar la función y asegurarse de que no se produzca un error
-	newFollower, err := useCase.FollowProfile(1, 2)
+	// Ejecutar la función y asegurarse de que se produzca un error
+	newFollower, err := useCase.FollowProfile(-1, -2)
 	if err != nil {
-		t.Errorf("Error inesperado: %v", err)
+		t.Logf("Expected error: %v", err)
+	} else {
+		t.Error("Expected an error, but got none.")
 	}
-	if newFollower == nil {
-		t.Error("newFollower es nulo, pero se esperaba un valor no nulo")
+
+	// Asegurarse de que newFollower es nulo
+	if newFollower != nil {
+		t.Error("newFollower is not null, but a null value was expected.")
 	}
 }
 
@@ -52,12 +56,54 @@ func TestIsFollowing(t *testing.T) {
 	repo := &MockFollowerRepository{}
 	useCase := NewFollowerUseCase(repo)
 
-	// Ejecutar la función y asegurarse de que no se produzca un error
+	// Caso 1: Verificar si el usuario 1 sigue al usuario 2 (esperando falso)
 	isFollowing, err := useCase.IsFollowing(1, 2)
 	if err != nil {
-		t.Errorf("Error inesperado: %v", err)
+		t.Errorf("Unexpected error: %v", err)
 	}
-	if isFollowing == nil {
-		t.Error("isFollowing es nulo, pero se esperaba un valor no nulo")
+	if *isFollowing != false {
+		t.Error("User 1 was expected not to follow user 2, but IsFollowing is true.")
+	}
+
+	// Caso 2: Verificar si el usuario 2 sigue al usuario 1 (esperando falso)
+	isFollowing, err = useCase.IsFollowing(2, 1)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	if *isFollowing != false {
+		t.Error("It was expected that user 2 would not follow user 1, but IsFollowing is true.")
+	}
+
+	// Caso 3: Verificar si el usuario -2 sigue al usuario 1 (esperando error)
+	_, err = useCase.IsFollowing(-2, 1)
+	if err == nil {
+		t.Error("An error was expected as the user provided is negative.")
+	} else {
+		expectedErrMsg := "Follower profile or followed profile values cannot be negative."
+		if err.Error() != expectedErrMsg {
+			t.Errorf("The error message '%s' was expected, but '%s' was obtained.", expectedErrMsg, err.Error())
+		}
+	}
+
+	// Caso 4: Verificar si el usuario 2 sigue al usuario -1 (esperando error)
+	_, err = useCase.IsFollowing(2, -1)
+	if err == nil {
+		t.Error("An error was expected as the user provided is negative.")
+	} else {
+		expectedErrMsg := "Follower profile or followed profile values cannot be negative."
+		if err.Error() != expectedErrMsg {
+			t.Errorf("The error message '%s' was expected, but '%s' was obtained.", expectedErrMsg, err.Error())
+		}
+	}
+
+	// Caso 5: Verificar si el usuario -2 sigue al usuario -1 (esperando error)
+	_, err = useCase.IsFollowing(-2, -1)
+	if err == nil {
+		t.Error("Se esperaba un error ya que el usuario proporcionado es negativo")
+	} else {
+		expectedErrMsg := "Follower profile or followed profile values cannot be negative."
+		if err.Error() != expectedErrMsg {
+			t.Errorf("The error message '%s' was expected, but '%s' was obtained.", expectedErrMsg, err.Error())
+		}
 	}
 }
