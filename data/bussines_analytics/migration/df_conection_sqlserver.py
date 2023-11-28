597c5625cfcb344239633325111608e0f344ff01
Patrickpisana23 <patrickpisana@unmsm.edu.pe>
2023-11-28 12:38:06 -0500

    new test conection

--- data/bussines_analytics/migration/df_conection_sqlserver.py
+++ data/bussines_analytics/migration/df_conection_sqlserver.py
@@ -1,34 +1,27 @@
 import pyodbc
+import time
 
 
-# Connection Testing
-try:
-    connection = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};'
-                                'Server=tcp:sv-socialdb-sql.database.windows.net,1433;'
-                                'Database=socialdb_sql;Uid=useradmin;Pwd= password ;Encrypt=yes;')
-    print("Conexión exitosa")
+user =''
+password =''
+server = ''
 
+def conexion():
+    try:
+        connection = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};'
+                                    'Server='+server+',1433;'
+                                    'Database=socialdb_sql;Uid='+user+';Pwd='+password+';Encrypt=yes;')
+        print("Conexión exitosa")
+        time.sleep(50000)
 
-    # Create a cursor
-    cursor = connection.cursor()
+        # Create a cursor
+        cursor = connection.cursor()
 
-    # Run a test query
-    cursor.execute("SELECT @@version;")
-    row = cursor.fetchone()
-    print("Versión del servidor de SQL Server: {}".format(row))
-
-    # Run a query on the 'bi_log' table
-    cursor.execute("SELECT * FROM bi_log")
-    rows = cursor.fetchall()
-    print(rows)
-    for row in rows:
-        print(row)
-
-except Exception as ex:
-    print(f"Error de conexión: {ex}")
-
-finally:
-    # Close the connection at completion
-    if connection:
-        connection.close()
-        print("Conexión cerrada")
+    except Exception as ex:
+        print(f"Error de conexión: {ex}")
+    
+    finally:
+        # Close the connection at completion
+        if connection:
+            connection.close()
+            print("Conexión cerrada")
