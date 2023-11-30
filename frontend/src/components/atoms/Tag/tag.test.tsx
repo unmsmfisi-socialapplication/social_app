1b5ed1d2e9032a6b1bd2981adb127e1322433caf
AnthonyRicse98 <73305764+AnthonyRicse98@users.noreply.github.com>
2023-11-29 22:47:39 -0500

    Delete file : repairt team frontend

--- frontend/src/components/atoms/Tag/tag.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, fireEvent } from '@testing-library/react'
-import WTag from './tag'
-import { AllInclusive, Home } from '@mui/icons-material'
-import { BrowserRouter } from 'react-router-dom'
-import './index.scss'
-
-describe('WTag', () => {
-
-})
