19b25aefdf943633548fbad642a73d984b3f82c5
Jeanpiere Julian Palacios Barrutia <87631120+JeanpierePalacios@users.noreply.github.com>
2023-11-30 13:41:39 -0500

    corrections pipeline

--- android/app/src/main/java/com/social/domain/model/CommentData.kt
+++
@@ -1,9 +0,0 @@
-package com.social.domain.model
-
-data class CommentData(
-    val username: String,
-    val time: String,
-    val reactionText: String,
-    val replyText: String,
-    val reactionCount: Int,
-)
