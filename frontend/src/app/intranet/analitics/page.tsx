bf52f85f011a98f1943f885acd3427e3911c9789
Ricse Anthony <anthony.ricse@unmsm.edu.pe>
2023-11-30 00:57:27 -0500

    fix (power bi ) : repair build dinamix next

--- frontend/src/app/intranet/analitics/page.tsx
+++ frontend/src/app/intranet/analitics/page.tsx
@@ -1,24 +1,33 @@
 'use client'
-import React from 'react'
-import { PowerBIEmbed } from 'powerbi-client-react'
+import React, { useEffect } from 'react'
+import dynamic from 'next/dynamic'
+import { EmbedProps } from 'powerbi-client-react'
 import IntranetHoc from '../intranet'
 import { Box } from '@mui/material'
 import './index.scss'
 const UrlConfig =
     'https://app.powerbi.com/view?r=eyJrIjoiMDI4OWExZGUtYjZlYS00YzFmLThkYjMtYzcwMTFjNDQ4ZGQ4IiwidCI6ImRmODY3OWNkLWE4MGUtNDVkOC05OWFjLWM4M2VkN2ZmOTVhMCJ9'
+const PowerBIEmbed = dynamic<EmbedProps>(() => import('powerbi-client-react').then((m) => m.PowerBIEmbed), {
+    ssr: false,
+})
 
 export default function index() {
+    useEffect(() => {}, [])
+
     return (
         <IntranetHoc sideBar>
             <h1>Estadisticas</h1>
+
             <Box className="container__bi">
-                <PowerBIEmbed
-                    cssClassName="container__bi--embed"
-                    embedConfig={{
-                        type: 'dashboard',
-                        embedUrl: UrlConfig,
-                    }}
-                />
+                {typeof window !== 'undefined' && (
+                    <PowerBIEmbed
+                        cssClassName="container__bi--embed"
+                        embedConfig={{
+                            type: 'dashboard',
+                            embedUrl: UrlConfig,
+                        }}
+                    />
+                )}
             </Box>
         </IntranetHoc>
     )
