9b99e0d9b7d18a3388a25e755fce7c6d703e8911
rodrigo1857 <rodrigo.vega@unmsm.edu.pe>
2023-12-01 17:22:25 -0500

    [FE] (Test): VerificationCode Test Component

--- frontend/src/components/molecules/VerificationCode/verificationCode.tsx
+++ frontend/src/components/molecules/VerificationCode/verificationCode.tsx
@@ -1,37 +1,39 @@
-import React from 'react'
-import WInputCode from '@/components/atoms/InputCode/inputCode'
+import React from 'react';
+import WInputCode from './../../../components/atoms/InputCode/inputCode';
 
 interface WVerificationCodeProps {
-    typeColor?: 'primary' | 'secondary'
-    placeholder?: string
-    size?: 'small' | 'medium'
-    variant?: 'standard' | 'filled' | 'outlined'
-    fullWidth?: boolean
-    type?: 'text' | 'password'
+  typeColor?: 'primary' | 'secondary';
+  placeholder?: string;
+  size?: 'small' | 'medium';
+  variant?: 'standard' | 'filled' | 'outlined';
+  fullWidth?: boolean;
+  type?: 'text' | 'password';
+  dataTestid?: string;
 }
 
 const WVerificationCode: React.FC<WVerificationCodeProps> = ({
-    typeColor = 'primary',
-    size = 'medium',
-    variant = 'outlined',
-    fullWidth = false,
-    type = 'text',
+  typeColor = 'primary',
+  size = 'medium',
+  variant = 'outlined',
+  fullWidth = false,
+  type = 'text',
+  dataTestid,
 }) => {
-    const divStyle = {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        gap: '30px',
-    }
+  const divStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '30px',
+  };
 
-    return (
-        <div style={divStyle}>
-            <WInputCode />
-            <WInputCode />
-            <WInputCode />
-            <WInputCode />
-        </div>
-    )
-}
+  return (
+    <div style={divStyle} data-testid={dataTestid}>
+      <WInputCode  />
+      <WInputCode  />
+      <WInputCode  />
+      <WInputCode  />
+    </div>
+  );
+};
 
-export default WVerificationCode
+export default WVerificationCode;
