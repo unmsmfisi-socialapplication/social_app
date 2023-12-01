dd8b59016e9f344086a026882cdc76c6f0f1513b
OscarYanfer <oscar.yanfer@unmsm.edu.pe>
2023-12-01 01:03:43 -0500

    add test buttonsquare

---
+++ frontend/src/components/atoms/ButtonSquare/buttonsquare.test.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import SquareButton from './ButtonSquare';
+
+describe('SquareButton Component', () => {
+  test('renders SquareButton correctly', () => {
+    render(<SquareButton />);
+    const outerButton = screen.getByTestId('outer-button');
+    expect(outerButton).toBeInTheDocument();
+  });
+});
