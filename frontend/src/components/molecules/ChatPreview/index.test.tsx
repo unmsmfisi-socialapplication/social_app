569887021a01929e43c853bf56f52b18610082cd
joseph-robles <joseph.robles@unmsm.edu.pe>
2023-11-30 21:42:42 -0500

    fix/test: remove snapshot testing

--- frontend/src/components/molecules/ChatPreview/index.test.tsx
+++ frontend/src/components/molecules/ChatPreview/index.test.tsx
@@ -3,13 +3,13 @@ import { render } from '@testing-library/react'
 import WChatPreview from './index'
 
 describe('WChatPreview Component Tests', () => {
-    // Prueba básica de renderizado
+    // Prueba de renderizado básico
     test('renderiza el componente WChatPreview', () => {
         render(<WChatPreview avatar="test-avatar" name="John Doe" messagePreview="Mensaje de prueba" time="12:34" />)
     })
 
-    // Prueba de renderizado personalizado con propiedades específicas
-    test('renderiza WChatPreview con propiedades personalizadas', () => {
+    // Prueba de renderizado personalizado con propiedades específicas y clases CSS
+    test('renderiza WChatPreview con propiedades y clases personalizadas', () => {
         const propiedadesPersonalizadas = {
             avatar: 'custom-avatar-url',
             name: 'John Doe',
@@ -17,37 +17,29 @@ describe('WChatPreview Component Tests', () => {
             time: '12:34',
         }
 
-        const { getByText } = render(<WChatPreview {...propiedadesPersonalizadas} />)
-
-        // Verifica si el contenido de texto específico está presente en el componente
-        expect(getByText(propiedadesPersonalizadas.name)).toBeInTheDocument()
-        expect(getByText(propiedadesPersonalizadas.messagePreview)).toBeInTheDocument()
-        expect(getByText(propiedadesPersonalizadas.time)).toBeInTheDocument()
-    })
-
-    // Prueba de snapshot
-    test('coincide con el snapshot', () => {
-        const { asFragment } = render(
-            <WChatPreview avatar="test-avatar" name="John Doe" messagePreview="Mensaje de prueba" time="12:34" />,
-        )
-        expect(asFragment()).toMatchSnapshot()
-    })
-
-    // Prueba de clases CSS
-    test('contiene las clases CSS esperadas', () => {
-        const propiedadesPersonalizadas = {
-            avatar: 'custom-avatar-url',
-            name: 'John Doe',
-            messagePreview: '¡Hola, mundo!',
-            time: '12:34',
-        }
-
-        const { container } = render(<WChatPreview {...propiedadesPersonalizadas} />)
+        const { container, getByText } = render(<WChatPreview {...propiedadesPersonalizadas} />)
 
         // Verifica si el contenedor principal tiene la clase CSS esperada
         expect(container.firstChild).toHaveClass('chatPreview')
 
         // Verifica si el avatar tiene la clase CSS esperada
         expect(container.querySelector('.avatar')).toHaveClass('avatar')
+
+        // Verifica si la información tiene la clase CSS esperada
+        expect(container.querySelector('.info')).toHaveClass('info')
+
+        // Verifica si el nombre tiene la clase CSS esperada
+        expect(container.querySelector('.name')).toHaveClass('name')
+
+        // Verifica si el mensaje tiene la clase CSS esperada
+        expect(container.querySelector('.message')).toHaveClass('message')
+
+        // Verifica si el tiempo tiene la clase CSS esperada
+        expect(container.querySelector('.time')).toHaveClass('time')
+
+        // Verifica si las propiedades están renderizadas correctamente
+        expect(getByText(propiedadesPersonalizadas.name)).toBeInTheDocument()
+        expect(getByText(propiedadesPersonalizadas.messagePreview)).toBeInTheDocument()
+        expect(getByText(propiedadesPersonalizadas.time)).toBeInTheDocument()
     })
 })
