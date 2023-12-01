94290c33bfb98fbf5f90ffd820ee8371a138ebae
francosilva25 <e.franco250299@gmail.com>
2023-12-01 18:47:36 -0500

    [Backend] feat: Unfollow Profile - Database Store Procedure

--- backend/migrations/0000000033_create_sp_unfollow_profile.up.sql
+++ backend/migrations/0000000033_create_sp_unfollow_profile.up.sql
@@ -8,7 +8,7 @@ LANGUAGE plpgsql
 AS $$
 BEGIN
     -- Insertar en la tabla SOC_APP_USER_PROFILE_UNFOLLOW
-    INSERT INTO sa.SOC_APP_USER_PROFILE_UNFOLLOW (
+    INSERT INTO SOC_APP_USER_PROFILE_UNFOLLOW (
         follower_profile_id,
         unfollowed_profile_id,
         unfollow_date
@@ -20,7 +20,7 @@ BEGIN
     );
 
     -- Eliminar de la tabla SOC_APP_USER_PROFILE_FOLLOW
-    DELETE FROM sa.SOC_APP_USER_PROFILE_FOLLOW 
+    DELETE FROM SOC_APP_USER_PROFILE_FOLLOW 
     WHERE
         follower_profile_id = p_follower_profile_id
         AND following_profile_id = p_unfollowed_profile_id;
