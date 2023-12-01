44a36aecee385be1736674317897ddbfa3778f4a
Adrianabalcd <adribalcd@gmail.com>
2023-12-01 14:37:10 -0500

    fix: fix mastodonClient

--- backend/internal/post/infrastructure/mastodonClient.go
+++ backend/internal/post/infrastructure/mastodonClient.go
@@ -1,14 +1,23 @@
 package infrastructure
 
 import (
-	"github.com/unmsmfisi-socialapplication/social_app/internal/post/domain"
+    "github.com/unmsmfisi-socialapplication/social_app/internal/post/domain"
+    // Importa paquetes adicionales necesarios para la API de Mastodon
 )
 
-// Mastodon
 type mastodonClient struct {
+    // Agrega campos necesarios para la configuración de la API
+}
 
+// NewMastodonClient crea una nueva instancia de MastodonClient
+func NewMastodonClient(/* parámetros de configuración */) domain.MastodonAPI {
+    return &mastodonClient{
+        // inicialización
+    }
 }
 
 func (m *mastodonClient) MultipostMastodon(post domain.PostCreate) error {
-    
+    // Implementa la lógica para publicar en Mastodon
+    // Por ejemplo, realizar una solicitud HTTP a la API de Mastodon
+    return nil
 }
