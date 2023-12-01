60a9c3496680baf4ef7e72c1e7141d839d99b3cf
Luis Aaron <71951748+aronlabr@users.noreply.github.com>
2023-12-01 12:45:58 -0500

    Add: Data ML resources to IAC

--- infra/data/main.tf
+++ infra/data/main.tf
@@ -1,6 +1,29 @@
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "example" {
   name     = "${var.environment}-${var.resource_group_name}"
   location = var.location
+}
+
+resource "azurerm_key_vault" "example" {
+  name                = "workspaceexamplekeyvault"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "premium"
+}
+
+resource "azurerm_machine_learning_workspace" "example" {
+  name                    = "example-workspace"
+  location                = azurerm_resource_group.example.location
+  resource_group_name     = azurerm_resource_group.example.name
+  application_insights_id = azurerm_application_insights.example.id
+  key_vault_id            = azurerm_key_vault.example.id
+  storage_account_id      = azurerm_storage_account.example.id
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
 resource "azurerm_storage_account" "example" {
