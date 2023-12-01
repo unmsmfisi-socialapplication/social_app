fe2d9a95495d37340e1f1312c476ee5988cbc535
Adrianabalcd <adribalcd@gmail.com>
2023-12-01 18:12:00 -0500

    feat: Add error handling for Pixelfed and Mastodon

--- backend/internal/post/infrastructure/mastodonClient.go
+++ backend/internal/post/infrastructure/mastodonClient.go
@@ -1,23 +1,27 @@
 package infrastructure
 
 import (
+    "log"
     "github.com/unmsmfisi-socialapplication/social_app/internal/post/domain"
-    // Importa paquetes adicionales necesarios para la API de Mastodon
 )
 
 type mastodonClient struct {
-    // Agrega campos necesarios para la configuración de la API
 }
 
-// NewMastodonClient crea una nueva instancia de MastodonClient
-func NewMastodonClient(/* parámetros de configuración */) domain.MastodonAPI {
+func NewMastodonClient() domain.MastodonAPI {
     return &mastodonClient{
-        // inicialización
     }
 }
 
 func (m *mastodonClient) MultipostMastodon(post domain.PostCreate) error {
-    // Implementa la lógica para publicar en Mastodon
-    // Por ejemplo, realizar una solicitud HTTP a la API de Mastodon
+   
+    var err error = nil
+
+    if err != nil {
+        log.Printf("Ocurrió un error al publicar en Mastodon: %v", err)
+        return err
+    }
+
     return nil
 }
+
--- backend/internal/post/infrastructure/pixelfedClient.go
+++ backend/internal/post/infrastructure/pixelfedClient.go
@@ -1,18 +1,25 @@
 package infrastructure
 
 import (
+    "log"
     "github.com/unmsmfisi-socialapplication/social_app/internal/post/domain"
 )
 
 type pixelfedClient struct {
 }
 
-func NewPixelfedClient(/* parámetros de configuración */) domain.PixelfedAPI {
-    return &pixelfedClient{
-    }
+func NewPixelfedClient() domain.PixelfedAPI {
+    return &pixelfedClient{}
 }
 
 func (p *pixelfedClient) MultipostPixelfeed(post domain.PostCreate) error {
+      var err error = nil
 
+    if err != nil {
+        log.Printf("Ocurrió un error al publicar en Pixelfed: %v", err)
+        return err
+    }
+    
     return nil
 }
+
