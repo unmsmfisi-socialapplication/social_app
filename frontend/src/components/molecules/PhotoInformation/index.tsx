891acc3cc8ae877f4970819748143f0c4d21d3c3
Cristhofer Ventura <cristhofer.ventura@unmsm.edu.pe>
2023-11-30 20:07:29 -0500

    add component photo information

---
+++ frontend/src/components/molecules/PhotoInformation/index.tsx
@@ -0,0 +1,43 @@
+'use client'
+import React from 'react'
+import styles from './index.module.scss'
+import { Box, Button, Card, Typography } from '@mui/material'
+import { WButton } from '@/components'
+
+interface PhotoInformationProps {
+    title?: string
+    open?: boolean
+}
+
+const WPhotoInformation: React.FC<PhotoInformationProps> = ({ title, open }) => {
+    return (
+        <Card className={styles.cardFollow}>
+            <div className={styles.header}>
+                <Button className={styles.closeButton}>X</Button>
+                <Typography variant="h6" component="div" className={styles.headerText}>
+                    {title}
+                </Typography>
+            </div>
+            <div className={styles.headerLine}></div>
+            <Typography variant="h6" component="div" className={styles.subtitle}>
+                Peso max(10 MB)
+            </Typography>
+            <Typography variant="h6" component="div" className={styles.subtitle}>
+                Tamaño max(851 x 315 px)
+            </Typography>
+            <Typography variant="h6" component="div" className={styles.subtitle}>
+                La plataforma acepta estas especificaciones para la foto de portada
+            </Typography>
+            <Box className="modal--report--submit" display="flex" justifyContent="center">
+                <WButton text={`Cargar foto`} />
+            </Box>
+        </Card>
+    )
+}
+
+export default WPhotoInformation
+
+WPhotoInformation.defaultProps = {
+    title: 'XokasXD',
+    open: false,
+}
