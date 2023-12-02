945d8f95235577860de56f506e6aaf4deb7990f5
Alexander Torre <alexander.torre.arteaga@gmail.com>
2023-12-01 19:48:03 -0500

    BE_Add_Test_post

--- backend/internal/profile/router.go
+++ backend/internal/profile/router.go
@@ -2,7 +2,6 @@ package profile
 
 import (
 	"database/sql"
-	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/unmsmfisi-socialapplication/social_app/internal/profile/application"
@@ -10,18 +9,13 @@ import (
 )
 
 func ProfileModuleRouter(dbInstance *sql.DB) *chi.Mux {
-    r := chi.NewRouter()
+	r := chi.NewRouter()
 
 	importProfileRepository := infrastructure.NewProfileRepository(dbInstance)
 	importProfileUseCase := application.NewImportProfileUseCase(importProfileRepository)
 	importProfileHandler := infrastructure.NewImportProfileHandler(importProfileUseCase)
 
-    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        // GET Profile
-        w.Write([]byte("Profile"))
-    })
-
 	r.Put("/import", importProfileHandler.ImportProfile)
 
-    return r
+	return r
 }
